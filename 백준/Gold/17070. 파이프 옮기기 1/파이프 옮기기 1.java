58a2a362be9c2570e84462d6ad88cd22cd22ca22
김진용 <88269663+jinyong3512@users.noreply.github.com>
2023-08-30 12:45:21 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 136 ms, Memory: 15260 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,106 +1,87 @@
-import java.io.*;
-import java.util.*;
+import java.util.*; // 유틸 라이브러리 임포트
+import java.io.*; // 입출력 라이브러리 임포트
 
-class Point {
-	int y;
-	int x;
+public class Main { // 메인 클래스
+	public static void main(String[] args) throws IOException { // 메인 클래스 실행
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 받는 라이브러리 객체 생성 초기화
+		StringBuilder sb = new StringBuilder(); // 출력 하는 라이브러리 객체 생성 초기화
+		StringTokenizer st; // 끊어 읽는 용도 라이브러리 객체 생성
 
-	Point(int y, int x) {
-		this.y = y;
-		this.x = x;
-	}
-}
+		int N;
+		int[][] arr;
 
-class Pipe {
-	Point head;
-	Point tail;
-	String direction;
-
-	Pipe(Point head, Point tail, String direction) {
-		this.head = head;
-		this.tail = tail;
-		this.direction = direction;
-	}
-}
-
-public class Main {
-
-	public static int answer = 0;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		int N = Integer.parseInt(br.readLine());
-		boolean[][] map = new boolean[N][N];
-
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				if (st.nextToken().equals("0"))
-					map[i][j] = false;
-				else
-					map[i][j] = true;
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		///////////////////////////////////////////
+		////////////////////////////////////////////////////
 
-		Pipe pipe = new Pipe(new Point(0, 1), new Point(1, 1), "horizontal");
-		recursion(map, pipe);
+		// 가로 세로 대각선
+		int[][][] dp = new int[N][N][3];
+		dp[0][1][0] = 1;
+
+		int answer = 0;
+		answer += recursion(arr, dp, N - 1, N - 1, 0);
+		answer += recursion(arr, dp, N - 1, N - 1, 1);
+		answer += recursion(arr, dp, N - 1, N - 1, 2);
+
+//		for (int k = 0; k < 3; k++) {
+//			for (int i = 0; i < N; i++) {
+//				for (int j = 0; j < N; j++) {
+//					System.out.print(dp[i][j][k] + " ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+//		}
+
 		System.out.println(answer);
 
 	}
 
-	public static void recursion(boolean[][] map, Pipe pipe) {
+	// 0 가로 1 세로 2대각선
+	public static int recursion(int[][] arr, int[][][] dp, int i, int j, int direction) {
+		if (i < 0 || j < 0)
+			return 0;
 
-		if (pipe.head.y == map.length - 1 && pipe.head.x == map[0].length - 1) {
-			answer++;
-			return;
+		if (arr[i][j] == 1)
+			return 0;
+
+		if (dp[i][j][direction] != 0)
+			return dp[i][j][direction];
+
+		// 내 머리가 해당 i,j이고 방향이 정해져 있을 때 구하고 싶어
+		// 해당 지점 가로 갯수는?
+		if (direction == 0) {
+			// 가로로 온거
+			dp[i][j][0] += recursion(arr, dp, i, j - 1, 0);
+
+			// 대각선이 회전한거
+			dp[i][j][0] += recursion(arr, dp, i, j - 1, 2);
+		}
+		// 세로
+		else if (direction == 1) {
+			// 세로로 온거
+			dp[i][j][1] += recursion(arr, dp, i - 1, j, 1);
+
+			// 대각선이 회전한거
+			dp[i][j][1] += recursion(arr, dp, i - 1, j, 2);
+		}
+		// 대각선
+		else {
+			if (i - 1 >= 0 && j - 1 >= 0 && arr[i][j - 1] != 1 && arr[i - 1][j] != 1) {
+				dp[i][j][2] += recursion(arr, dp, i - 1, j - 1, 0);
+				dp[i][j][2] += recursion(arr, dp, i - 1, j - 1, 1);
+				dp[i][j][2] += recursion(arr, dp, i - 1, j - 1, 2);
+			}
 		}
 
-		// 가로 방향
-		if (pipe.direction.equals("horizontal")) {
-			// 가로로 보내기
-			if (pipe.head.x + 1 < map[0].length && !map[pipe.head.y][pipe.head.x + 1])
-				recursion(map, new Pipe(new Point(pipe.head.y, pipe.head.x + 1),
-						new Point(pipe.tail.y, pipe.tail.x + 1), "horizontal"));
-			// 대각선 보내기
-			if (pipe.head.y + 1 < map.length && pipe.head.x + 1 < map[0].length && !map[pipe.head.y + 1][pipe.head.x]
-					&& !map[pipe.head.y][pipe.head.x + 1] && !map[pipe.head.y + 1][pipe.head.x + 1])
-				recursion(map, new Pipe(new Point(pipe.head.y + 1, pipe.head.x + 1),
-						new Point(pipe.tail.y, pipe.tail.x + 1), "diagonal"));
-		}
+		return dp[i][j][direction];
 
-		// 세로 방향
-		else if (pipe.direction.equals("vertical")) {
-			// 세로로 보내기
-			if (pipe.head.y + 1 < map.length && !map[pipe.head.y + 1][pipe.head.x])
-				recursion(map, new Pipe(new Point(pipe.head.y + 1, pipe.head.x),
-						new Point(pipe.tail.y + 1, pipe.tail.x), "vertical"));
-			// 대각선 보내기
-			if (pipe.head.y + 1 < map.length && pipe.head.x + 1 < map[0].length && !map[pipe.head.y + 1][pipe.head.x]
-					&& !map[pipe.head.y][pipe.head.x + 1] && !map[pipe.head.y + 1][pipe.head.x + 1])
-				recursion(map, new Pipe(new Point(pipe.head.y + 1, pipe.head.x + 1),
-						new Point(pipe.tail.y + 1, pipe.tail.x), "diagonal"));
-		}
-
-		// 대각선 방향
-		else if (pipe.direction.equals("diagonal")) {
-			// 가로로 보내기
-			if (pipe.head.x + 1 < map[0].length && !map[pipe.head.y][pipe.head.x + 1])
-				recursion(map, new Pipe(new Point(pipe.head.y, pipe.head.x + 1),
-						new Point(pipe.tail.y + 1, pipe.tail.x + 1), "horizontal"));
-			// 세로로 보내기
-			if (pipe.head.y + 1 < map.length && !map[pipe.head.y + 1][pipe.head.x])
-				recursion(map, new Pipe(new Point(pipe.head.y + 1, pipe.head.x),
-						new Point(pipe.tail.y + 1, pipe.tail.x + 1), "vertical"));
-			// 대각선 보내기
-			if (pipe.head.y + 1 < map.length && pipe.head.x + 1 < map[0].length && !map[pipe.head.y + 1][pipe.head.x]
-					&& !map[pipe.head.y][pipe.head.x + 1] && !map[pipe.head.y + 1][pipe.head.x + 1])
-				recursion(map, new Pipe(new Point(pipe.head.y + 1, pipe.head.x + 1),
-						new Point(pipe.tail.y + 1, pipe.tail.x+1), "diagonal"));
-		}
 	}
 }
