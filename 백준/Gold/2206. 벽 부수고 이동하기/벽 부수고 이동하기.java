5a6019f8bbb0084c82ff4c185c930546e2e71036
김진용 <88269663+jinyong3512@users.noreply.github.com>
2023-09-08 22:36:58 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 520 ms, Memory: 85312 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -3,17 +3,13 @@ import java.util.*;
 
 class Point2 {
 	int r, c, value;
-	int flag;
-	// flag가 0 -> 기회안 쓴애
-	// flag가 1 -> 기회 쓴애
-	// boolean flag 에서 바꾼 이유 visited[0][1] 인덱스 그대로 쓰려고
-	// boolean 쓰면 if(flag) visited[1]에 저장 이런식으로 한번 더 if문을 써야 할 것 같아서
+	int use_chance_count;
 
-	Point2(int r, int c, int value, int flag) {
+	Point2(int r, int c, int value, int use_chance_count) {
 		this.r = r;
 		this.c = c;
 		this.value = value;
-		this.flag = flag;
+		this.use_chance_count = use_chance_count;
 	}
 }
 
@@ -31,7 +27,6 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int[][] map = new int[N][M];
-		boolean[][][] visited = new boolean[N][M][2];
 
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
@@ -43,15 +38,22 @@ public class Main {
 		//////////////////////////////////////////////////////////////
 
 		Queue<Point2> que = new ArrayDeque<>();
+		int[][] point_use_chance = new int[N][M];
+		// 찬스 1번 쓴애가 해당 지점에 가면 큐에 넣을꺼임 근데 0번 쓴애가 해당 지점에가면 큐에 또 넣을꺼임 그러니 2로 초기화
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				point_use_chance[i][j] = 2;
+			}
+		}
 
 		que.offer(new Point2(0, 0, 1, 0)); // 시작점과 한칸째라고 쓰고 맨 처음에는 부시기 기회 안 쓴애~
-		visited[0][0][0] = true; // visited[0]은 부시기 기회 안쓴애로 쓰기로 약속 visited[1]은 부시기 쓴애로 약속
+		point_use_chance[0][0] = 0; // 0 -> 찬스 안쓴애가 방문 했었습니다~
 
 		while (!que.isEmpty()) {
 			Point2 curr = que.poll();
 
 			// 도착 지점 종료
-			if (curr.r == N-1 && curr.c == M-1) {
+			if (curr.r == N - 1 && curr.c == M - 1) {
 				sb.append(curr.value);
 				bw.write(sb.toString());
 				bw.flush();
@@ -66,45 +68,20 @@ public class Main {
 
 				// 칸 안에 있어?
 				if (-1 < row && row < N && -1 < col && col < M) {
-					// 나 찬스 있어!
-					if (curr.flag == 0) {
-						// 찬스 쓸꺼야!
-						if (!visited[row][col][1] && map[row][col] == 1) {
-							visited[row][col][1] = true;
-							que.offer(new Point2(row, col, curr.value + 1, 1));
-						}
-						// 찬스 안써
-						if (!visited[row][col][0] && map[row][col] == 0) {
-							visited[row][col][0] = true;
-							que.offer(new Point2(row, col, curr.value + 1, 0));
+					// 찬스 있어
+					if (curr.use_chance_count == 0) {
+						// 찬스 쓸래
+						if (point_use_chance[row][col] > curr.use_chance_count + 1 && map[row][col] == 1) {
+							point_use_chance[row][col] = curr.use_chance_count + 1;
+							que.add(new Point2(row, col, curr.value + 1, curr.use_chance_count + 1));
 						}
 					}
-
-					// 나 찬스 없음!
-					else {
-						// 찬스 쓸꺼야!
-						// 안돼
-
-						// 찬스 안써
-						if (!visited[row][col][1] && map[row][col] == 0) {
-							visited[row][col][1] = true;
-							que.offer(new Point2(row, col, curr.value + 1, 1));
-						}
+					// 찬스 있던 없던 안쓴다
+					if (point_use_chance[row][col] > curr.use_chance_count && map[row][col] == 0) {
+						point_use_chance[row][col] = curr.use_chance_count;
+						que.add(new Point2(row, col, curr.value + 1, curr.use_chance_count));
 					}
 
-//					// 찬스 쓴애야 || 부실벽이 없음
-//					if (curr.flag || map[row][col] == 0) {
-//						visited[row][col][0] = true;
-//						que.offer(new Point2(row, col, curr.value + 1, curr.flag));
-//					} else { // 벽 부실 기회 있음
-//						if (map[row][col] == 1) { // 부신다
-//							visited[row][col][0] = true;
-//							visited[row][col][1] = true;
-//							que.offer(new Point2(row, col, curr.value + 1, true));
-//						} else { // 안 부신다
-//							que.offer(new Point2(row, col, curr.value + 1, curr.flag));
-//						}
-//					}
 				}
 
 			}
