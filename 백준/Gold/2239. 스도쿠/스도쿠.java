7d14e7caf81d17079c5e09ea3ff769bb4fc4a3a1
김진용 <88269663+jinyong3512@users.noreply.github.com>
2023-10-04 10:26:16 +0900

    [Gold IV] Title: 스도쿠, Time: 1288 ms, Memory: 19116 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -18,66 +19,50 @@ public class Main {
 
 		//////////////////////////////////////////////////////
 
-		// 가로
-		boolean[][] i_visited = new boolean[9][10];
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				i_visited[i][arr[i][j]] = true;
-			}
-		}
-		// 세로
-		boolean[][] j_visited = new boolean[9][10];
-		for (int j = 0; j < 9; j++) {
-			for (int i = 0; i < 9; i++) {
-				j_visited[j][arr[i][j]] = true;
-			}
-		}
-
-		// 사각형
-		boolean[][][] visited = new boolean[3][3][10];
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				for (int y = 0; y < 3; y++) {
-					for (int x = 0; x < 3; x++) {
-						visited[i][j][arr[3 * i + y][3 * j + x]] = true;
-					}
-				}
-			}
-		}
-
-		recursion(arr, i_visited, j_visited, visited, 0);
+		recursion(arr, 0);
 	}
 
-	public static void recursion(int[][] arr, boolean[][] i_visited, boolean[][] j_visited, boolean[][][] visited,
-			int index) {
+	public static void recursion(int[][] arr, int index) {
 		if (index == 81) {
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr[0].length; j++) {
 					System.out.print(arr[i][j]);
 				}
-				System.out.println("");
+				System.out.println();
 			}
 			System.exit(0);
 		}
 
-		if (arr[index / 9][index % 9] == 0) {
-			for (int num = 1; num <= 9; num++) {
-				if (!i_visited[index / 9][num] && !j_visited[index % 9][num]
-						&& !visited[index / 9 / 3][index % 9 / 3][num]) {
-					i_visited[index / 9][num] = true;
-					j_visited[index % 9][num] = true;
-					visited[index / 9 / 3][index % 9 / 3][num] = true;
-					arr[index / 9][index % 9] = num;
-					recursion(arr, i_visited, j_visited, visited, index + 1);
-					i_visited[index / 9][num] = false;
-					j_visited[index % 9][num] = false;
-					visited[index / 9 / 3][index % 9 / 3][num] = false;
-					arr[index / 9][index % 9] = 0;
+		if (arr[index / arr.length][index % arr.length] == 0) {
+			for (int i = 1; i <= 9; i++) {
+				boolean can = true;
+
+				for (int y = 0; y < 9; y++) {
+					if (arr[y][index % arr.length] == i)
+						can = false;
 				}
+
+				for (int x = 0; x < 9; x++) {
+					if (arr[index / arr.length][x] == i)
+						can = false;
+				}
+
+				for (int y = 0; y < 3; y++) {
+					for (int x = 0; x < 3; x++) {
+						if (arr[index / arr.length / 3 * 3 + y][index % arr.length / 3 * 3 + x] == i)
+							can = false;
+					}
+				}
+
+				if (can) {
+					arr[index / arr.length][index % arr.length] = i;
+					recursion(arr, index + 1);
+					arr[index / arr.length][index % arr.length] = 0;
+				}
+
 			}
-		}
-		else {
-			recursion(arr, i_visited, j_visited, visited, index + 1);
+		} else {
+			recursion(arr, index + 1);
 		}
 	}
 }
