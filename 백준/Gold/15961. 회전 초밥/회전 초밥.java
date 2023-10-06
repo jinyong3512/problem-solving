a80bcad295ed0afc6ca29509574eb749272eb49b
김진용 <88269663+jinyong3512@users.noreply.github.com>
2023-10-06 10:22:20 +0900

    [Gold IV] Title: 회전 초밥, Time: 504 ms, Memory: 168748 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,51 +1,50 @@
-import java.util.*; // 유틸 라이브러리 임포트
-import java.io.*; // 입출력 라이브러리 임포트
+import java.io.*;
+import java.util.*;
 
-//
+public class Main {
 
-public class Main { // 메인 클래스
-	public static void main(String[] args) throws IOException { // 메인 클래스 실행
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 받는 라이브러리 객체 생성 초기화
-		StringBuilder sb = new StringBuilder(); // 출력 하는 라이브러리 객체 생성 초기화
-		StringTokenizer st; // 끊어 읽는 용도 라이브러리 객체 생성
-
-		int N, d, k, c;
-		int[] arr;
-
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-
-		for (int i = 0; i < N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
-
-		///////////////////////////////////////////////
-		HashMap<Integer, Integer> hashMap = new HashMap<>();
-		hashMap.put(c, 1);
-
-		int answer = 0;
-		int left = 0;
-		int right = 0;
-
-		for (; right < k; right++)
-			hashMap.put(arr[right], hashMap.getOrDefault(arr[right], 0) + 1);
-
-		for (; left < N; left++) {
-			if (hashMap.get(arr[left]) == 1)
-				hashMap.remove(arr[left]);
-			else {
-				hashMap.put(arr[left], hashMap.get(arr[left]) - 1);
-			}
-
-			hashMap.put(arr[right % N], hashMap.getOrDefault(arr[right % N], 0) + 1);
-			right++;
-
-			answer = Math.max(answer, hashMap.size());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int N = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int[] eaten = new int[D + 1];
+		int max = 1;
+		eaten[C] = 1;
+		int[] table = new int[N + K - 1];
+		for (int i = 0; i < N; i++) {
+			table[i] = Integer.parseInt(br.readLine().trim());
+		}
+		for (int i = 0; i < K - 1; i++) {
+			table[N + i] = table[i];
 		}
 
-		System.out.println(answer);
+		for (int i = 0; i < K; i++) {
+			if (eaten[table[i]] == 0) {
+				max++;
+			}
+			eaten[table[i]]++;
+		}
+		int s = 0;
+		int tmp = max;
+		for (int i = K; i < N + K - 1; i++) {
+
+			if (eaten[table[i]] == 0) {
+				tmp++;
+
+			}
+			eaten[table[i]]++;
+			if (eaten[table[s]] == 1) {
+				tmp--;
+			}
+			eaten[table[s]]--;
+			s++;
+			max = Math.max(max, tmp);
+		}
+		System.out.println(max);
+
 	}
+
 }
