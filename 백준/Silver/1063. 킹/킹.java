024b42e10595bef73f205a0f8593c70cdff5ece4
김진용 (JINYONG KIM) <88269663+jinyong3512@users.noreply.github.com>
2023-12-07 12:12:56 +0900

    [Silver III] Title: 킹, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.java
+++ 백준/Silver/1063. 킹/킹.java
@@ -2,127 +2,87 @@ import java.io.*;
 import java.util.*;
 
 class Point {
-	int y, x;
+    int y, x;
 
-	Point(int y, int x) {
-		this.y = y;
-		this.x = x;
-	}
+    Point(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
 }
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+public class Main {
 
-		st = new StringTokenizer(br.readLine());
-		String input_king = st.nextToken();
-		String input_rock = st.nextToken();
-		int N = Integer.parseInt(st.nextToken());
+    public static int[] dy = new int[]{0, 0, 1, -1, -1, -1, 1, 1};
+    public static int[] dx = new int[]{1, -1, 0, 0, 1, -1, 1, -1};
 
-		Point king = new Point(7 - (input_king.charAt(1) - '1'), input_king.charAt(0) - 'A');
-		Point rock = new Point(7 - (input_rock.charAt(1) - '1'), input_rock.charAt(0) - 'A');
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("R", 0);
+        hashMap.put("L", 1);
+        hashMap.put("B", 3);
+        hashMap.put("T", 2);
+        hashMap.put("RT", 6);
+        hashMap.put("LT", 7);
+        hashMap.put("RB", 4);
+        hashMap.put("LB", 5);
 
-			Point new_king = king;
-			Point new_rock = rock;
+        st = new StringTokenizer(br.readLine());
 
-			if (line.equals("R")) {
-				new_king = new Point(king.y, king.x + 1);
+        int y,x;
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y, rock.x + 1);
-				}
-			} else if (line.equals("L")) {
-				new_king = new Point(king.y, king.x - 1);
+        String str = st.nextToken();
+        x = str.charAt(0)-'A';
+        y = str.charAt(1)-'1';
+        Point kingPoint = new Point(y,x);
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y, rock.x - 1);
-				}
+//        System.out.println(kingPoint.y + " "+kingPoint.x);
 
-			} else if (line.equals("B")) {
-				new_king = new Point(king.y + 1, king.x);
+        str = st.nextToken();
+        x = str.charAt(0)-'A';
+        y = str.charAt(1)-'1';
+        Point rockPoint = new Point(y,x);
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y + 1, rock.x);
-				}
+//        System.out.println(rockPoint.y + " " + rockPoint.x);
 
-			} else if (line.equals("T")) {
-				new_king = new Point(king.y - 1, king.x);
+        int N = Integer.parseInt(st.nextToken());
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y - 1, rock.x);
-				}
+        //////////////////////////////////////////////////////////////////////////
 
-			} else if (line.equals("RT")) {
-				new_king = new Point(king.y - 1, king.x + 1);
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y - 1, rock.x + 1);
-				}
+            int direction = hashMap.get(line);
 
-			} else if (line.equals("LT")) {
-				new_king = new Point(king.y - 1, king.x - 1);
+            int newY = kingPoint.y + dy[direction];
+            int newX = kingPoint.x + dx[direction];
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y - 1, rock.x - 1);
-				}
+            if (newY >= 0 && newY < 8 && newX >= 0 && newX < 8) {
 
-			} else if (line.equals("RB")) {
-				new_king = new Point(king.y + 1, king.x + 1);
+                if (newY == rockPoint.y && newX == rockPoint.x) {
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y + 1, rock.x + 1);
-				}
+                    if (rockPoint.y + dy[direction] >= 0 && rockPoint.y + dy[direction] < 8 && rockPoint.x + dx[direction] >= 0 && rockPoint.x + dx[direction] < 8) {
+                        kingPoint.y += dy[direction];
+                        kingPoint.x += dx[direction];
+                        rockPoint.y += dy[direction];
+                        rockPoint.x += dx[direction];
+                    }
 
-			} else if (line.equals("LB")) {
-				new_king = new Point(king.y + 1, king.x - 1);
+                }else{
+                    kingPoint.y += dy[direction];
+                    kingPoint.x += dx[direction];
+                }
+            }
 
-				// 같은 칸이다!
-				if (new_king.y == rock.y && new_king.x == rock.x) {
-					new_rock = new Point(rock.y + 1, rock.x - 1);
-				}
+        }
 
-			}
+        sb.append((char) (kingPoint.x + 'A')).append((char) (kingPoint.y + '1')).append("\n")
+                .append((char) (rockPoint.x + 'A')).append((char) (rockPoint.y + '1'));
 
-			if (new_king.y > 7 || new_king.y < 0)
-				continue;
-			else if (new_king.x > 7 || new_king.x < 0)
-				continue;
+        System.out.println(sb);
 
-			else if (new_rock.y > 7 || new_rock.y < 0)
-				continue;
-			else if (new_rock.x > 7 || new_rock.x < 0)
-				continue;
-			else {
-				king = new_king;
-				rock = new_rock;
-			}
-
-		}
-//		
-//		Point king = new Point(7 - (input_king.charAt(1) - '1'), input_king.charAt(0) - 'A');
-//		Point rock = new Point(7 - (input_rock.charAt(1) - '1'), input_rock.charAt(0) - 'A');
-
-//		king.y = 7 - (정답-'1');
-		
-		System.out.print((char) (king.x + 'A'));
-		System.out.println((char) (7 - king.y + '1'));
-
-		System.out.print((char) (rock.x + 'A'));
-		System.out.println((char) (7 - rock.y + '1'));
-		
-		
-
-	}
+    }
 }
