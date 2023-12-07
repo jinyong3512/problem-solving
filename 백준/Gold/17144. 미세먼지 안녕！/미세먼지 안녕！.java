e96642196f2fa04753befb9743ff95c1ae68c14d
김진용 (JINYONG KIM) <88269663+jinyong3512@users.noreply.github.com>
2023-12-07 16:20:49 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 352 ms, Memory: 31460 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,14 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	public static void main(String[] args) throws IOException {
+class Point {
+	int y, x;
+
+	Point(int y, int x) {
+		this.y = y;
+		this.x = x;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
 		int R, C, T;
 		int[][] arr;
+
+		///////////////////////////////////////////////////////////
 
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
@@ -23,132 +34,154 @@ class Main {
 			}
 		}
 
-		/////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////
 
-		int cleaner_head_i = -1;
-		int cleaner_tail_i = -1;
+		Point head = null;
+		Point tail = null;
 
-		for (int i = 0; i < R; i++) {
-			if (arr[i][0] == -1) {
-				cleaner_head_i = i;
-				break;
-			}
-		}
-		for (int i = R - 1; i >= 0; i--) {
-			if (arr[i][0] == -1) {
-				cleaner_tail_i = i;
-				break;
-			}
-		}
-
-		for (int t = 0; t < T; t++) {
-			// 1. 미세먼지가 확산된다. 확산은 미세먼지가 있는 모든 칸에서 동시에 일어난다.
-			int[][] new_arr = new int[R][C];
-			spread_of_fine_dust(arr, new_arr);
-
-			for (int i = 0; i < R; i++) {
-				for (int j = 0; j < C; j++) {
-					arr[i][j] = new_arr[i][j];
-				}
-			}
-
-			// 2. 공기청정기가 작동한다.
-
-			// 대가리 왼쪽
-			for (int i = cleaner_head_i - 1; i >= 0; i--) {
-				arr[i + 1][0] = arr[i][0];
-			}
-
-			// 대가리 위
-			for (int j = 1; j < arr[0].length; j++) {
-				arr[0][j - 1] = arr[0][j];
-			}
-
-			// 대가리 오른쪽
-			for (int i = 1; i <= cleaner_head_i; i++) {
-				arr[i - 1][arr[0].length - 1] = arr[i][arr[0].length - 1];
-			}
-
-			// 대가리 아래
-			for (int j = arr[0].length - 2; j >= 0; j--) {
-				arr[cleaner_head_i][j + 1] = arr[cleaner_head_i][j];
-			}
-
-			arr[cleaner_head_i][0] = -1;
-			arr[cleaner_head_i][1] = 0;
-
-			// 꼬리 왼쪽
-			for (int i = cleaner_tail_i + 1; i < arr.length; i++) {
-				arr[i - 1][0] = arr[i][0];
-			}
-
-			// 꼬리 아래
-			for (int j = 1; j < arr[0].length; j++) {
-				arr[arr.length - 1][j - 1] = arr[arr.length - 1][j];
-			}
-
-			// 꼬리 오른쪽
-			for (int i = arr.length - 1 - 1; i >= cleaner_tail_i; i--) {
-				arr[i + 1][arr[0].length - 1] = arr[i][arr[0].length - 1];
-			}
-
-			// 꼬리 위
-			for (int j = arr[0].length - 1 - 1; j >= 0; j--) {
-				arr[cleaner_tail_i][j + 1] = arr[cleaner_tail_i][j];
-			}
-
-			arr[cleaner_tail_i][0] = -1;
-			arr[cleaner_tail_i][1] = 0;
-
-			
-
-		}
-
-		int count = 0;
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (arr[i][j] > 0)
-					count += arr[i][j];
-			}
-		}
-		System.out.println(count);
-	}
-
-	public static void spread_of_fine_dust(int[][] arr, int[][] new_arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				if (arr[i][j] > 0) {
-					int count = 0;
+				if (arr[i][j] == -1 && head == null) {
+					head = new Point(i, j);
+				} else if (arr[i][j] == -1 && head != null) {
+					tail = new Point(i, j);
+				}
+			}
+		}
 
-					// 위
+		for (int time = 0; time < T; time++) {
+			arr = spread(arr);
+
+//			for (int i = 0; i < arr.length; i++) {
+//				for (int j = 0; j < arr[0].length; j++) {
+//					System.out.print(arr[i][j] + " ");
+//				}
+//				System.out.println("");
+//			}
+
+			clean(arr, head, tail);
+
+//			for (int i = 0; i < arr.length; i++) {
+//				for (int j = 0; j < arr[0].length; j++) {
+//					System.out.print(arr[i][j] + " ");
+//				}
+//				System.out.println("");
+//			}
+
+		}
+
+		long answer = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				answer += arr[i][j];
+			}
+
+		}
+		System.out.println(answer + 2);
+
+	}
+
+	public static int[][] spread(int[][] arr) {
+		int[][] new_arr = new int[arr.length][arr[0].length];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				if (arr[i][j] == -1) {
+					new_arr[i][j] = -1;
+				} else if (arr[i][j] == 0) {
+
+				} else {
+
+					int count_can_dust_spread = 0;
+					// 위로
 					if (i - 1 >= 0 && arr[i - 1][j] != -1) {
-						count++;
 						new_arr[i - 1][j] += arr[i][j] / 5;
+						count_can_dust_spread++;
 					}
 
-					// 아래
+					// 아래로
 					if (i + 1 < arr.length && arr[i + 1][j] != -1) {
-						count++;
 						new_arr[i + 1][j] += arr[i][j] / 5;
+						count_can_dust_spread++;
 					}
 
 					// 왼쪽
 					if (j - 1 >= 0 && arr[i][j - 1] != -1) {
-						count++;
 						new_arr[i][j - 1] += arr[i][j] / 5;
+						count_can_dust_spread++;
 					}
 
 					// 오른쪽
 					if (j + 1 < arr[0].length && arr[i][j + 1] != -1) {
-						count++;
 						new_arr[i][j + 1] += arr[i][j] / 5;
+						count_can_dust_spread++;
 					}
 
-					new_arr[i][j] += arr[i][j] - (arr[i][j] / 5) * (count);
-
+					new_arr[i][j] += arr[i][j] - (arr[i][j] / 5) * count_can_dust_spread;
 				}
 			}
 		}
+
+		return new_arr;
+
 	}
 
+	public static void clean(int[][] arr, Point head, Point tail) {
+		int remember_left_top = arr[0][0];
+
+		// 상단
+		for (int x = 0; x < arr[0].length - 1; x++) {
+			arr[0][x] = arr[0][x + 1];
+		}
+
+		// 우측
+		for (int y = 0; y < head.y; y++) {
+			arr[y][arr[0].length - 1] = arr[y + 1][arr[0].length - 1];
+		}
+
+		// 하단
+		for (int x = arr[0].length - 1; x > 0; x--) {
+			arr[head.y][x] = arr[head.y][x - 1];
+		}
+
+		// 좌측
+		for (int y = head.y; y > 0; y--) {
+			arr[y][0] = arr[y - 1][0];
+		}
+
+		arr[1][0] = remember_left_top;
+
+		arr[head.y][head.x] = -1;
+		arr[head.y][head.x + 1] = 0;
+
+		///////////////////////////////////////////////////////////
+
+		int remember_right_top = arr[tail.y][arr[0].length - 1];
+
+		// 상단
+		for (int x = arr[0].length - 1; x > 0; x--) {
+			arr[tail.y][x] = arr[tail.y][x - 1];
+		}
+
+		// 좌측
+		for (int y = tail.y; y < arr.length - 1; y++) {
+			arr[y][0] = arr[y + 1][0];
+		}
+
+		// 하단
+		for (int x = 0; x < arr[0].length - 1; x++) {
+			arr[arr.length - 1][x] = arr[arr.length - 1][x + 1];
+		}
+
+		// 우측
+		for (int y = arr.length - 1; y > tail.y; y--) {
+			arr[y][arr[0].length - 1] = arr[y - 1][arr[0].length - 1];
+		}
+
+		arr[tail.y + 1][arr[0].length - 1] = remember_right_top;
+
+		arr[tail.y][tail.x] = -1;
+		arr[tail.y][tail.x + 1] = 0;
+	}
 }
