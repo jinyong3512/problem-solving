69c3c0a2ccb707eda175b586612230b7927b1e1c
김진용 (JINYONG KIM) <88269663+jinyong3512@users.noreply.github.com>
2023-12-11 03:17:39 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T;
+        T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            sb.append(dp[N]).append("\n");
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
