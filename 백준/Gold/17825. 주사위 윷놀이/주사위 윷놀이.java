068c77e79363fe25b5b53bdef09bf7c090e18e0b
김진용 (JINYONG KIM) <88269663+jinyong3512@users.noreply.github.com>
2023-12-11 04:53:24 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 140 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
@@ -1,142 +1,147 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+// 말은 게임판에 그려진 화살표의 방향대로만 이동할 수 있다.
+// 말이 파란색 칸에서 이동을 시작하면 파란색 화살표를 타야 하고, 이동하는 도중이거나 파란색이 아닌 칸에서 이동을 시작하면 빨간색 화살표를 타야 한다.
+// 말이 도착 칸으로 이동하면 주사위에 나온 수와 관계 없이 이동을 마친다.
 
-class Point {
-    int index;
-    int score;
-
-    Point(int index, int score) {
-        this.index = index;
-        this.score = score;
-    }
-}
-
+// ans : 얻을 수 있는 점수의 최댓값
 public class Main {
 
-    public static int answer = Integer.MIN_VALUE;
+    static int ans;
+    static int[][] board = {{2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40},
+            {10, 13, 16, 19, 25, 30, 35, 40},
+            {20, 22, 24, 25, 30, 35, 40},
+            {30, 28, 27, 26, 25, 30, 35, 40}};
+    static int[] dice = new int[10];
+    static Horse[] horses;
+
+    static class Horse {
+        int y, x;
+        boolean goal = false;
+
+        public Horse(int y, int x) {
+            this.y = y;
+            this.x = x;
+
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-
-        // 1:10 시작
-
-        ArrayList<ArrayList<Point>> graph = new ArrayList<>();
-        // 0 출발
-        // 1 부터 20까지는 밖
-        // 21 도착
-        // 가로 5 -> 22 -> 23 ->  24 ->  25  <- 26 <- 27 <- 28 <- 15
-        // 세로 10 29 30  |  25  | 31 32  | 20 21
-        for (int i = 0; i <= 32; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        // 0 ->1 , 1 ->2 ....... 19 -> 20
-        for (int i = 0; i < 20; i++)
-            graph.get(i).add(new Point(i + 1, 2 * (i + 1)));
-
-        graph.get(20).add(new Point(21, 0));
-
-        // 가로
-        graph.get(5).add(new Point(22, 13));
-        graph.get(22).add(new Point(23, 16));
-        graph.get(23).add(new Point(24, 19));
-        graph.get(24).add(new Point(25, 25));
-
-        graph.get(15).add(new Point(28, 28));
-        graph.get(28).add(new Point(27, 27));
-        graph.get(27).add(new Point(26, 26));
-        graph.get(26).add(new Point(25, 25));
-
-        // 세로
-        graph.get(10).add(new Point(29, 22));
-        graph.get(29).add(new Point(30, 24));
-        graph.get(30).add(new Point(25, 25));
-
-        graph.get(25).add(new Point(31, 30));
-        graph.get(31).add(new Point(32, 35));
-        graph.get(32).add(new Point(20, 40));
-
-
-        ///////////////////////////////////////////////////////////////////
-
-        int score = 0;
-        int[] horses = new int[4];
-        int[] dices = new int[10];
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < 10; i++) {
-            dices[i] = Integer.parseInt(st.nextToken());
+            dice[i] = Integer.parseInt(st.nextToken());
+        }
+        horses = new Horse[4];
+        for (int i = 0; i < 4; i++) {
+            horses[i] = new Horse(0, -1);
         }
 
-        recursion(graph, dices, 0, 0, horses);
+        solve(0, 0);
 
-        System.out.println(answer);
+        System.out.println(ans);
 
     }
 
-    public static void recursion(ArrayList<ArrayList<Point>> graph, int[] dices, int diceIndex, int score, int[] horses) {
-        if (diceIndex == 10) {
-            answer = Math.max(answer, score);
+    public static void solve(int depth, int point) {
 
+        if (depth == 10) {
+            // 계산
+            ans = Math.max(ans, point);
             return;
         }
 
-        answer = Math.max(answer, score);
+//        ans = Math.max(ans, point);
 
-
-
-        // 한말씩 이동해보자
         for (int i = 0; i < 4; i++) {
+            if (!horses[i].goal) {
 
-            // 끝 말 선택하면 pass
-            if (horses[i] == 21)
-                continue;
+                int nx = horses[i].x + dice[depth];
+                int ny = horses[i].y;
 
-            int curIndex = horses[i];
-            int diceNumber = dices[diceIndex];
-            int tmpScore = score;
+                if (nx >= board[ny].length) {
+                    horses[i].goal = true; // 도착
+                    solve(depth + 1, point);
+                    horses[i].goal = false;
+                } else {
+                    // 10 일 경우
+                    if (ny == 0 && nx == 4) {
+                        ny = 1;
+                        nx = 0;
+                    }
+                    // 20일 경우
+                    else if (ny == 0 && nx == 9) {
+                        ny = 2;
+                        nx = 0;
+                    }
+                    // 30일 경우
+                    else if (ny == 0 && nx == 14) {
+                        ny = 3;
+                        nx = 0;
+                    }
+                    // 40 공유해주기
+                    else if (ny == 0 && nx == 19) {
+                        ny = 3;
+                        nx = 7;
+                    } else if (ny == 1 && nx == 7) {
+                        ny = 3;
+                        nx = 7;
+                    } else if (ny == 2 && nx == 6) {
+                        ny = 3;
+                        nx = 7;
+                    }
+                    // 35
+                    else if (ny == 1 && nx == 6) {
+                        ny = 3;
+                        nx = 6;
+                    } else if (ny == 2 && nx == 5) {
+                        ny = 3;
+                        nx = 6;
+                    }
+                    // 30
+                    else if (ny == 1 && nx == 5) {
+                        ny = 3;
+                        nx = 5;
+                    } else if (ny == 2 && nx == 4) {
+                        ny = 3;
+                        nx = 5;
+                    }
+                    // 25
+                    else if (ny == 1 && nx == 4) {
+                        ny = 3;
+                        nx = 4;
+                    } else if (ny == 2 && nx == 3) {
+                        ny = 3;
+                        nx = 4;
+                    }
 
-            if (curIndex == 5 || curIndex == 10 || curIndex == 15) {
 
-                diceNumber--;
-                if (diceNumber == 0)
-                    tmpScore += graph.get(curIndex).get(1).score;
+                    boolean check = true;
+                    for (int j = 0; j < 4; j++) {
+                        if (!horses[j].goal && i != j && horses[j].y == ny && horses[j].x == nx) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (!check)
+                        continue;
 
-                curIndex = graph.get(curIndex).get(1).index;
-            }
+                    int tx = horses[i].x;
+                    int ty = horses[i].y;
+                    horses[i].x = nx;
+                    horses[i].y = ny;
+                    solve(depth + 1, point + board[ny][nx]);
+                    horses[i].x = tx;
+                    horses[i].y = ty;
 
-            for (int j = 0; j < diceNumber; j++) {
-                if (curIndex == 21)
-                    break;
-
-                if (diceNumber - 1 == j) {
-                    tmpScore += graph.get(curIndex).get(0).score;
                 }
 
-                curIndex = graph.get(curIndex).get(0).index;
-            }
-
-
-            boolean can = true;
-            for (int j = 0; j < 4; j++) {
-                if (curIndex == horses[j] && horses[j] != 21)
-                    can = false;
-            }
-
-            if (can) {
-                int[] tmpHorses = new int[4];
-                for (int j = 0; j < 4; j++) {
-                    tmpHorses[j] = horses[j];
-                }
-
-                tmpHorses[i] = curIndex;
-                recursion(graph, dices, diceIndex + 1, tmpScore, tmpHorses);
             }
         }
 
-
     }
+
 }
