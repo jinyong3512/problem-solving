ae951e055fd350dbe8b4f6e6885d1a43e0439a55
김진용 (JINYONG KIM) <88269663+jinyong3512@users.noreply.github.com>
2023-12-20 18:11:50 +0900

    [Silver I] Title: 1로 만들기 2, Time: 116 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,19 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Data {
-    int value;
-    int depth;
-    ArrayList<Integer> arrayList = new ArrayList<>();
-
-    Data(int value, int depth) {
-        this.value = value;
-        this.depth = depth;
-    }
-}
-
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -22,67 +10,41 @@ public class Main {
         int N;
 
         N = Integer.parseInt(br.readLine());
+
         ///////////////////////////////////////////////////////////////////
 
         int[] dp = new int[N + 1];
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++)
-            graph.add(new ArrayList<>());
-
         for (int i = 1; i <= N; i++)
             dp[i] = Integer.MAX_VALUE;
-        dp[N] = 0;
+        dp[1] = 0;
 
-        for (int i = N; i >= 2; i--) {
-            if (i % 3 == 0) {
-                graph.get(i).add(i / 3);
-                dp[i / 3] = Math.min(dp[i / 3], dp[i] + 1);
+        int[] prev = new int[N + 1];
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            prev[i] = i - 1;
+
+            if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
+                dp[i] = dp[i / 2] + 1;
+                prev[i] = i / 2;
             }
-            if (i % 2 == 0) {
-                graph.get(i).add(i / 2);
-                dp[i / 2] = Math.min(dp[i / 2], dp[i] + 1);
+            if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
+                dp[i] = dp[i / 3] + 1;
+                prev[i] = i / 3;
             }
-            graph.get(i).add(i - 1);
-            dp[i - 1] = Math.min(dp[i - 1], dp[i] + 1);
         }
 
-        sb.append(dp[1]).append("\n");
+        sb.append(dp[N]).append("\n");
 
-        boolean[] visited = new boolean[N + 1];
-        Queue<Data> queue = new LinkedList<>();
-
-        visited[N] = true;
-        Data data = new Data(N, 0);
-        data.arrayList.add(N);
-        queue.add(data);
-
-        while (!queue.isEmpty()) {
-            Data tmp = queue.remove();
-
-            if (tmp.value == 1) {
-                for (int i = 0; i < tmp.arrayList.size(); i++) {
-                    sb.append(tmp.arrayList.get(i) + " ");
-                }
-                break;
-            }
-
-            for (int i = 0; i < graph.get(tmp.value).size(); i++) {
-                if (!visited[graph.get(tmp.value).get(i)]) {
-                    visited[graph.get(tmp.value).get(i)] = true;
-                    Data newData = new Data(graph.get(tmp.value).get(i), tmp.depth + 1);
-                    for (int v : tmp.arrayList) {
-                        newData.arrayList.add(v);
-                    }
-                    newData.arrayList.add(graph.get(tmp.value).get(i));
-                    queue.add(newData);
-                }
-            }
-
+        int value = N;
+        sb.append(value).append(" ");
+        while (value != 1) {
+            value = prev[value];
+            sb.append(value).append(" ");
         }
 
         System.out.println(sb);
-
 
     }
 }
