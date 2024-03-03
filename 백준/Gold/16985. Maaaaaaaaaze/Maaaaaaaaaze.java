713347b37a170d51907a8e268b127e066e0d4876
김진용 (Kim Jinyong) <88269663+jinyong3512@users.noreply.github.com>
2024-03-03 16:50:18 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 3076 ms, Memory: 324156 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
@@ -1,126 +1,166 @@
-
 import java.io.*;
 import java.util.*;
-import java.util.Map.Entry;
+
+class Point {
+    int z, y, x, depth;
+
+    Point(int z, int y, int x, int depth) {
+        this.z = z;
+        this.y = y;
+        this.x = x;
+        this.depth = depth;
+    }
+}
 
 public class Main {
-	static int[][][] maze = new int[5][5][5];
-	static int ans;
-	static int[] order = new int[5];
-	static boolean[] selected = new boolean[5];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		for (int h = 0; h < 5; h++) {
-			for (int i = 0; i < 5; i++) {
-				st = new StringTokenizer(br.readLine().trim());
-				for (int j = 0; j < 5; j++) {
-					maze[h][i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-		}
-		ans = Integer.MAX_VALUE;
-		escape(0);
-		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+    public static int answer = Integer.MAX_VALUE;
 
-	}
+    public static int[] dz = new int[]{-1, 1, 0, 0, 0, 0};
+    public static int[] dy = new int[]{0, 0, -1, 1, 0, 0};
+    public static int[] dx = new int[]{0, 0, 0, 0, -1, 1};
 
-	static public int bfs() {
-		if ( maze[order[0]][0][0] == 0) {
-			return -1;
-		}
-		int[] dx = { 0, -1, 0, 1 };
-		int[] dy = { -1, 0, 1, 0 };
-		int[] dh = { -1, 1 };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		boolean[][][] visited = new boolean[5][5][5];
-		visited[0][0][0] = true;
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] { 0, 0, 0, 0 });
-//		for (int h = 0; h < 5; h++) {
-//			for (int i = 0; i < 5; i++) {
-//				System.out.println(Arrays.toString(maze[h][i]));
-//			}
-//		}
-		while (!q.isEmpty()) {
-			int now[] = q.poll();
-			int h = now[0], y = now[1], x = now[2], cnt = now[3];
-			if (h == 4 && y == 4 && x == 4) {
-				return cnt;
+        // 15:55
+        // 0은 못감 1은 감(=하얀색)
 
-			}
-			for (int d = 0; d < 2; d++) {
-				int nh = h + dh[d];
-				if (nh < 0 || nh >= 5 || visited[nh][y][x] || maze[order[nh]][y][x] == 0) {
-					continue;
-				}
-				visited[nh][y][x] = true;
-				q.add(new int[] { nh, y, x, cnt + 1 });
-			}
-			for (int d = 0; d < 4; d++) {
-				int ny = y + dy[d];
-				int nx = x + dx[d];
-				if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5 || visited[h][ny][nx] || maze[order[h]][ny][nx] == 0) {
-					continue;
-				}
-				visited[h][ny][nx] = true;
-				q.add(new int[] { h, ny, nx, cnt + 1 });
-			}
-		}
+        boolean[][][] map = new boolean[5][5][5];
 
-		return -1;
-	}
+        for (int k = 0; k < 5; k++) {
+            for (int i = 0; i < 5; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 5; j++) {
+                    if (st.nextToken().equals("1")) {
+                        map[k][i][j] = true;
+                    }
+                }
+            }
+        }
 
-	static public void rotateRight(int h) {
-		int[][] Temp = new int[5][5];
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				Temp[i][j] = maze[order[h]][5 - j - 1][i];
-			}
-		}
+        ////////////////////////////////////////////////
 
-		maze[order[h]] = Temp.clone();
+        // 각 판을 회전시키는 경우의 수
+        // 다시 랜덤으로 5개 P
+        // 통과 해보기
 
-	}
+        Deque<Integer> deque = new ArrayDeque<>();
+        boolean[] visited = new boolean[5];
+        permutation(map, visited, deque);
+
+        if (answer == Integer.MAX_VALUE)
+            System.out.println("-1");
+        else
+            System.out.println(answer);
+    }
+
+    public static void rotate(boolean[][] board) {
+        boolean[][] newBoard = new boolean[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                newBoard[i][j] = board[board.length - 1 - j][i];
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = newBoard[i][j];
+            }
+        }
+    }
+
+    public static void permutation(boolean[][][] map, boolean[] visited, Deque<Integer> deque) {
+        if (deque.size() == 5) {
+
+            boolean[][][] newMap = new boolean[5][5][5];
+            int k = 0;
+            for (Integer number : deque) {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        newMap[k][i][j] = map[number][i][j];
+                    }
+                }
+                k++;
+            }
+            recursion(newMap, 0);
+
+            return;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (!visited[i]) {
+                deque.add(i);
+                visited[i] = true;
+                permutation(map, visited, deque);
+                visited[i] = false;
+                deque.removeLast();
+            }
+        }
+    }
+
+    public static void recursion(boolean[][][] map, int index) {
+
+        if (index == 5) {
+
+            bfs(map, 0, 0, 0, 4, 4, 4);
+            bfs(map, 0, 4, 0, 4, 0, 4);
+            bfs(map, 0, 0, 4, 4, 4, 0);
+            bfs(map, 0, 4, 4, 4, 0, 0);
+
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            recursion(map, index + 1);
+            rotate(map[index]);
+        }
+
+    }
+
+    public static void bfs(boolean[][][] map, int startZ, int startY, int startX, int endZ, int endY, int endX) {
+
+        if (!map[startZ][startY][startX])
+            return;
+
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(startZ, startY, startX, 0));
+
+        boolean[][][] visited = new boolean[5][5][5];
+        visited[startZ][startY][startX] = true;
+
+        while (!queue.isEmpty()) {
+            Point tmp = queue.remove();
+
+            if (tmp.z == endZ && tmp.y == endY && tmp.x == endX) {
+                answer = Math.min(answer, tmp.depth);
+                break;
+            }
+
+            for (int direction = 0; direction < 6; direction++) {
+                int newZ = tmp.z + dz[direction];
+                int newY = tmp.y + dy[direction];
+                int newX = tmp.x + dx[direction];
+
+                if (newZ < 0 || newY < 0 || newX < 0 || newZ >= 5 || newY >= 5 || newX >= 5)
+                    continue;
+
+                if (visited[newZ][newY][newX])
+                    continue;
+
+                if (!map[newZ][newY][newX])
+                    continue;
+
+                visited[newZ][newY][newX] = true;
+                queue.add(new Point(newZ, newY, newX, tmp.depth + 1));
+
+            }
 
 
+        }
 
-	static public void rotate(int idx) {
 
-		if (idx == 5) {
-
-			int res = bfs();
-			if (res != -1) {
-				ans = Math.min(ans, res);
-			}
-			return;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			rotateRight(order[idx]);
-			
-			
-			rotate(idx + 1);
-		}
-		
-
-	}
-
-	static public void escape(int cnt) {
-		if (cnt == 5) {
-			rotate(0);
-			return;
-		}
-		for (int i = 0; i < 5; i++) {
-			if (!selected[i]) {
-				selected[i] = true;
-				order[cnt] = i;
-				escape(cnt + 1);
-				selected[i] = false;
-			}
-
-		}
-	}
-
+    }
 }
