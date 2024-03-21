cd1872b627b63385cb28c5240386e5548a99becd
김진용 (Kim Jinyong) <88269663+jinyong3512@users.noreply.github.com>
2024-03-21 17:32:43 +0900

    [Gold IV] Title: RGB거리 2, Time: 88 ms, Memory: 12208 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -7,80 +7,75 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        // 비용의 최솟값
-        // 0.5 초니까 5000만 초
-
-        // 1번 집 2번 N번 집 과 색 달라
-        // N번집 N-1번과 1번집 색 달라야 함
-        // 원형이란 얘기임
-
-        // 앞뒤 집의 색과 달라야 함 ㅇㅇ
-
-        // 10^3 *10^3 = 10^6 비용 long 안해도 됌
+        // N 2 ~ 1000
 
         int N;
-        int[][] arr;
+        int[][] costs;
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][3];
+        costs = new int[N][3];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
+            costs[i][0] = Integer.parseInt(st.nextToken());
+            costs[i][1] = Integer.parseInt(st.nextToken());
+            costs[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        ////////////////////////////////////////////////
+        ///////////////////////////////////////////////////
 
         int answer = Integer.MAX_VALUE;
 
+        // 0번 출발 했다고 가정
         long[][] dp = new long[N][3];
 
-        dp[0][0] = arr[0][0];
+        dp[0][0] = costs[0][0];
         dp[0][1] = Integer.MAX_VALUE;
         dp[0][2] = Integer.MAX_VALUE;
 
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
 
-        answer = (int) Math.min(answer, dp[N - 1][1]);
-        answer = (int) Math.min(answer, dp[N - 1][2]);
+        answer = Math.min(answer, (int) Math.min(dp[N - 1][1], dp[N - 1][2]));
 
 
+        // 1번 출발 했다고 가정
         dp = new long[N][3];
 
         dp[0][0] = Integer.MAX_VALUE;
-        dp[0][1] = arr[0][1];
+        dp[0][1] = costs[0][1];
         dp[0][2] = Integer.MAX_VALUE;
 
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
 
-        answer = (int) Math.min(answer, dp[N - 1][0]);
-        answer = (int) Math.min(answer, dp[N - 1][2]);
+        answer = Math.min(answer, (int) Math.min(dp[N - 1][0], dp[N - 1][2]));
 
+
+        // 2번 출발 했다고 가정
         dp = new long[N][3];
 
         dp[0][0] = Integer.MAX_VALUE;
         dp[0][1] = Integer.MAX_VALUE;
-        dp[0][2] = arr[0][2];
+        dp[0][2] = costs[0][2];
 
-        for (int i = 1; i < N ; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
 
-        answer = (int) Math.min(answer, dp[N - 1][0]);
-        answer = (int) Math.min(answer, dp[N - 1][1]);
+        answer = Math.min(answer, (int) Math.min(dp[N - 1][0], dp[N - 1][1]));
 
 
         System.out.println(answer);
+
+
     }
 }
