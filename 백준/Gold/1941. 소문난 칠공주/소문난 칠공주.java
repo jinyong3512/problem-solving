47f3248426a79d759e8f9451f30b4191acb2eef3
김진용 (Kim Jinyong) <88269663+jinyong3512@users.noreply.github.com>
2024-03-24 23:19:24 +0900

    [Gold III] Title: 소문난 칠공주, Time: 244 ms, Memory: 134456 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,75 +1,118 @@
+import java.io.*;
 import java.util.*;
- 
- 
+
+class Point {
+    int y, x;
+
+    Point(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
+
 public class Main {
-    static char map[][] = new char[5][5];
-    static int dx[] = {-1,1,0,0};
-    static int dy[] = {0,0,-1,1};
-    static int combX[] = new int[25];
-    static int combY[] = new int[25];
-    static int ans = 0;
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for(int i=0; i<5; i++) {
-            map[i] = sc.next().toCharArray();
+
+    public static int[] dy = new int[]{-1, 1, 0, 0};
+    public static int[] dx = new int[]{0, 0, -1, 1};
+
+    public static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        // 7명 구성
+        // 세로가로 반드시 인접
+        // 아무 학생이나
+        // 그러나 S가 4명 이상이여야 함
+
+
+        char[][] students = new char[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < 5; j++) {
+                students[i][j] = line.charAt(j);
+            }
         }
-        
-        
-        /* 좌표 미리 계산 */
-        for(int i=0; i<25; i++) {
-            combX[i] = i % 5;
-            combY[i] = i / 5;
-        }
-        
-        combination(new int[7], 0,0,7);
-        System.out.println(ans);
-        
-    }
-    
-    public static void combination(int[] comb, int idx, int depth, int left ) {
-        if(left == 0) {
-            bfs(comb);
-            return;
-        }
-        
-        if(depth == 25) return;
-        
-        comb[idx] = depth;
-        combination(comb, idx+1, depth+1, left-1);    // 선택한 경우
-        combination(comb, idx, depth+1, left);    // 선택하지 않은 경우
-    }
-    
-    public static void bfs(int comb[]) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean visited[] = new boolean[7];
-        
-        visited[0] = true;
-        q.add(comb[0]);
-        int cnt = 1, sCnt = 0;
-        
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            if(map[combY[cur]][combX[cur]] == 'S') sCnt++;
-            
-            for(int i=0; i<4; i++) {
-                for(int next=1; next<7; next++) {
-                    if(!visited[next] && combX[cur]+dx[i] == combX[comb[next]] && combY[cur]+dy[i] == combY[comb[next]]) {
-                        visited[next] = true;
-                        q.add(comb[next]);
-                        cnt++;
-                    }
+
+        //////////////////////////////////////////////////////////////////////
+
+        boolean[] visited = new boolean[25];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (students[i][j] == 'S') {
+                    combination(students, i * 5 + j, 1, 1, 0, visited);
+                } else {
+                    combination(students, i * 5 + j, 1, 0, 1, visited);
                 }
             }
         }
-        
-        /* 7개 모두 연결되어 있다면 */
-        if(cnt == 7) {
-            if(sCnt >=4) {
-                ans++;
+
+        System.out.println(answer);
+    }
+
+    public static void combination(char[][] students, int index, int depth, int sNumber, int yNumber, boolean[] visited) {
+        visited[index] = true;
+
+        if (yNumber >= 4) {
+
+            visited[index] = false;
+            return;
+        }
+
+        if (depth == 7) {
+
+            int curY = index / 5;
+            int curX = index % 5;
+
+            Queue<Point> queue = new LinkedList<>();
+            queue.add(new Point(curY, curX));
+
+            boolean[][] curVisited = new boolean[5][5];
+            curVisited[curY][curX] = true;
+
+            int count = 1;
+            while (!queue.isEmpty()) {
+                Point curPoint = queue.remove();
+
+                for (int direction = 0; direction < 4; direction++) {
+                    int newY = curPoint.y + dy[direction];
+                    int newX = curPoint.x + dx[direction];
+
+                    if (newY < 0 || newY >= 5 || newX < 0 || newX >= 5) {
+                        continue;
+                    }
+
+                    if (curVisited[newY][newX])
+                        continue;
+
+                    if (visited[newY * 5 + newX]) {
+                        count++;
+                        curVisited[newY][newX] = true;
+                        queue.add(new Point(newY, newX));
+                    }
+                }
+            }
+
+            if (count == 7) {
+                answer++;
+            }
+
+
+            visited[index] = false;
+            return;
+        }
+        for (int i = index + 1; i < 25; i++) {
+            if (students[i / 5][i % 5] == 'S') {
+                combination(students, i, depth + 1, sNumber + 1, yNumber, visited);
+            } else {
+                combination(students, i, depth + 1, sNumber, yNumber + 1, visited);
             }
         }
-        
+        visited[index] = false;
+
     }
- 
 }
