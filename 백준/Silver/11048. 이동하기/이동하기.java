8de72b675f012864a14cc6d52ac48ed8e92111b3
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-16 02:36:59 +0900

    [Silver II] Title: 이동하기, Time: 536 ms, Memory: 80908 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -2,53 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		int N, M;
-		int[][] arr;
+    public static int[] dy = new int[]{1, 0, 1};
+    public static int[] dx = new int[]{0, 1, 1};
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        int N, M;
+        int[][] arr;
 
-		///////////////////////////////////////////////////
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		int[][] dp = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++)
-				dp[i][j] = -1;
-		}
+        arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		System.out.println(recursion(arr, dp, N - 1, M - 1));
+        /////////////////////////////////////////////////////
 
-//		for (int i = 0; i < N; i++) {
-//			for (int j = 0; j < M; j++)
-//				System.out.print(dp[i][j] + " ");
-//			System.out.println();
-//		}
+        long[][] dp = new long[N][M];
+        for (int i = 0; i < N; i++) {
 
-	}
+            // j =0;
+            dp[i][0] = arr[i][0];
+            if (i - 1 >= 0)
+                dp[i][0] += dp[i - 1][0];
 
-	public static int recursion(int[][] arr, int[][] dp, int i, int j) {
-		if (i < 0 || j < 0)
-			return 0;
+            for (int j = 1; j < M; j++) {
 
-		if (dp[i][j] != -1)
-			return dp[i][j];
+                if(i== 0){
+                   dp[i][j] = dp[i][j-1] + arr[i][j];
+                }
+                else{
+                    dp[i][j] = Math.max(Math.max(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]) + arr[i][j];
+                }
 
-		return dp[i][j] = Math.max(Math.max(recursion(arr, dp, i - 1, j), recursion(arr, dp, i, j - 1)),
-				recursion(arr, dp, i - 1, j - 1)) + arr[i][j];
+            }
+        }
 
-	}
+        System.out.println(dp[N-1][M-1]);
 
+    }
 }
