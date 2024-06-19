27564c3cb2621cdca0faf15d274b69251e483f26
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-19 11:20:37 +0900

    [Gold IV] Title: 비슷한 단어, Time: 260 ms, Memory: 19676 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,106 +1,76 @@
 import java.io.*;
 import java.util.*;
 
-class Data {
-	String word;
-	int init_index;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-	Data(String word, int init_index) {
-		this.word = word;
-		this.init_index = init_index;
-	}
-}
+        int N;
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> tmpArrayList = new ArrayList<>();
 
-class Data2 {
-	String word;
-	int index1;
-	int index2;
+        N = Integer.parseInt(br.readLine());
 
-	Data2(String word, int index1, int index2) {
-		this.word = word;
-		this.index1 = index1;
-		this.index2 = index2;
-	}
-}
+        for (int i = 0; i < N; i++) {
+            String tmpStr = br.readLine();
+            arrayList.add(tmpStr);
+            tmpArrayList.add(tmpStr);
+        }
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+        /////////////////////////////////////////////////
 
-		int N;
-		String[] words;
+        HashMap<String, Integer> hashMap = new HashMap<>();
 
-		N = Integer.parseInt(br.readLine());
-		words = new String[N];
-		for (int i = 0; i < N; i++)
-			words[i] = br.readLine();
+        Collections.sort(tmpArrayList);
 
-		////////////////////////////////////////////
+        int maxM = -1;
+        for (int i = 0; i < tmpArrayList.size() - 1; i++) {
 
-		Data[] datas = new Data[N];
-		for (int i = 0; i < N; i++) {
-			datas[i] = new Data(words[i], i);
-		}
+            String curStr = tmpArrayList.get(i);
+            String nextStr = tmpArrayList.get(i + 1);
 
-		Arrays.sort(datas, new Comparator<Data>() {
-			@Override
-			public int compare(Data o1, Data o2) {
-				return o1.word.compareTo(o2.word);
-			}
-		});
+            int curM = 0;
+            for (int j = 0; j < curStr.length() && j < nextStr.length(); j++) {
+                if (curStr.charAt(j) == nextStr.charAt(j)) {
+                    curM++;
+                } else {
+                    break;
+                }
+            }
 
-		ArrayList<Data2> arrayList = new ArrayList<>();
-		arrayList.add(new Data2("", Integer.MAX_VALUE, Integer.MAX_VALUE));
+            if (maxM > curM) {
+                continue;
+            } else if (maxM == curM) {
+                hashMap.put(curStr.substring(0, maxM), 0);
+            } else {
+                maxM = curM;
+                hashMap.clear();
+                hashMap.put(curStr.substring(0, maxM), 0);
+            }
+        }
 
-		for (int i = 0; i < N - 1; i++) {
-			String word1 = datas[i].word;
-			String word2 = datas[i + 1].word;
+        String S = "";
+        String T = "";
 
-			String tmp_head = "";
-			for (int word_index = 0; word_index < Math.min(word1.length(), word2.length()); word_index++) {
-				if (word1.charAt(word_index) == word2.charAt(word_index)) {
-					tmp_head += word1.charAt(word_index);
-				} else {
-					break;
-				}
-			}
+        for (int i = 0; i < arrayList.size(); i++) {
 
-			if (arrayList.get(0).word.length() > tmp_head.length()) {
+            if( arrayList.get(i).length() < maxM)
+                continue;
 
-			} else if (arrayList.get(0).word.length() == tmp_head.length()) {
-				arrayList.add(new Data2(tmp_head, datas[i].init_index, datas[i + 1].init_index));
-			} else {
-				arrayList.clear();
-				arrayList.add(new Data2(tmp_head, datas[i].init_index, datas[i + 1].init_index));
-			}
-		}
+            if (hashMap.containsKey(arrayList.get(i).substring(0, maxM))) {
+                if(S.isEmpty()){
+                    hashMap.clear();
+                    hashMap.put(arrayList.get(i).substring(0, maxM), 0);
+                    S = arrayList.get(i);
+                }else{
+                    T = arrayList.get(i);
+                    break;
+                }
+            }
+        }
+        System.out.println(S+"\n"+T);
 
-		int S = Integer.MAX_VALUE;
-		int arrayList_S_index = -1;
-		for (int i = 0; i < arrayList.size(); i++) {
-			if (S > arrayList.get(i).index1) {
-				S = arrayList.get(i).index1;
-				arrayList_S_index = i;
-			}
-			if (S > arrayList.get(i).index2) {
-				S = arrayList.get(i).index2;
-				arrayList_S_index = i;
-			}
-		}
-
-		int T = Integer.MAX_VALUE;
-		for (int i = 0; i < arrayList.size(); i++) {
-			if (arrayList.get(i).word.equals(arrayList.get(arrayList_S_index).word)) {
-				if (S != arrayList.get(i).index1)
-					T = Math.min(T, arrayList.get(i).index1);
-				if (S != arrayList.get(i).index2)
-					T = Math.min(T, arrayList.get(i).index2);
-			}
-		}
-
-		System.out.println(words[S]);
-		System.out.println(words[T]);
-	}
+    }
 }
