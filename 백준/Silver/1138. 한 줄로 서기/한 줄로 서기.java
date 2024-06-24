dd24d6af8c0e373f20a1709ed35e64f3eb83ab45
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-25 08:28:57 +0900

    [Silver II] Title: 한 줄로 서기, Time: 152 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -24,43 +24,73 @@ public class Main {
         }
 
         //////////////////////////////////////////////
-        realAnswer = new int[N];
-        int[] answer = new int[N];
-        boolean[] visited = new boolean[N];
-        permutation(N, arr, answer, visited, 0);
+        // 방법 1
+//        realAnswer = new int[N];
+//        int[] answer = new int[N];
+//        boolean[] visited = new boolean[N];
+//        permutation(N, arr, answer, visited, 0);
+//
+//        for(int i =0 ; i < N ; i++){
+//            sb.append(realAnswer[i]).append(" ");
+//        }
+//        System.out.println(sb);
 
-        for(int i =0 ; i < N ; i++){
-            sb.append(realAnswer[i]).append(" ");
+        // 방법 2
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            int count = arr[i];
+
+            int curCount = 0;
+            for (int j = 0; j < N; j++) {
+
+                // 더 큰키가 들어갈 곳이다.
+                if (answer[j] == 0) {
+                    if (curCount == count) {
+                        answer[j] = i + 1;
+                        break;
+                    } else {
+                        curCount++;
+                    }
+                }
+                // 더 작은 키가 이미 들어가 있다.
+                else {
+                    continue;
+                }
+            }
         }
+
+        for(int i = 0 ; i < N ; i++)
+            sb.append(answer[i]+" ");
         System.out.println(sb);
+
     }
 
     public static void permutation(int N, int[] arr, int[] answer, boolean[] visited, int curIndex) {
 
-        if(curIndex == N){
+        if (curIndex == N) {
 
-            for(int i = 0 ; i < N ; i++){
+            for (int i = 0; i < N; i++) {
                 int count = 0;
-                for(int j = 0 ; j < i ; j++){
-                    if(answer[j] > answer[i])
+                for (int j = 0; j < i; j++) {
+                    if (answer[j] > answer[i])
                         count++;
                 }
-                if(count != arr[answer[i]])
+                if (count != arr[answer[i]])
                     return;
             }
 
-            for(int i =0 ; i < N ; i++){
-                realAnswer[i] = answer[i]+1;
+            for (int i = 0; i < N; i++) {
+                realAnswer[i] = answer[i] + 1;
             }
 
             return;
         }
 
-        for(int i = 0 ; i < N ; i++){
-            if(!visited[i]){
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
                 answer[curIndex] = i;
-                permutation(N,arr,answer,visited,curIndex+1);
+                permutation(N, arr, answer, visited, curIndex + 1);
                 visited[i] = false;
             }
         }
