411a7f2b6962af4e88286ea9f0e6b79a2c8758fa
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-25 09:29:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 300 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,96 +1,99 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Point {
-	int y, x;
+class Vertex {
 
-	Point(int y, int x) {
-		this.y = y;
-		this.x = x;
-	}
+    int y, x, weight;
+
+    Vertex(int y, int x, int weight) {
+        this.y = y;
+        this.x = x;
+        this.weight = weight;
+    }
+
 }
 
+
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
-		/*
-		 * dfs로 매번 확인해야 한다 하지만 dfs 자체가 겹치는 부분이 너무 많음 dp다
-		 */
+    public static int[] dy = new int[]{-1, 1, 0, 0};
+    public static int[] dx = new int[]{0, 0, -1, 1};
 
-		int test_case = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		while (true) {
+        int t = 0;
+        while(true){
+            t++;
 
-			test_case++;
+            int N;
+            int[][] arr;
 
-			int N;
-			int[][] arr;
+            N = Integer.parseInt(br.readLine());
+            if(N==0)
+                break;
 
-			//////////////////////////////////////////////////////////////////
+            arr = new int[N][N];
+            for(int i =0 ; i < N ; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < N ; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-			N = Integer.parseInt(br.readLine());
+            /////////////////////////////////////////////////////
 
-			if (N == 0)
-				break;
+            int[][] distances = dijkstra(arr, 0, 0);
+            sb.append("Problem ").append(t).append(": ").append(distances[N-1][N-1]).append("\n");
 
-			arr = new int[N][N];
+        }
 
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+        System.out.println(sb);
 
-			//////////////////////////////////////////////////////////////////
+    }
 
-			// 해당 i,j 지점을 방문할때 최솟값!!!
-			long[][] dp = new long[N][N];
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					dp[i][j] = Integer.MAX_VALUE;
-				}
-			}
+    public static int[][] dijkstra(int[][] arr, int startY, int startX) {
 
-			dp[0][0] = arr[0][0];
+        PriorityQueue<Vertex> pQ = new PriorityQueue<Vertex>(new Comparator<Vertex>() {
+            @Override
+            public int compare(Vertex o1, Vertex o2) {
+                return o1.weight - o2.weight;
+            }
+        });
 
-			Queue<Point> queue = new LinkedList<>();
-			queue.add(new Point(0, 0));
+        boolean[][] visited = new boolean[arr.length][arr[0].length];
+        int[][] distances = new int[arr.length][arr[0].length];
 
-			while (!queue.isEmpty()) {
-				Point tmp = queue.remove();
+        pQ.add(new Vertex(startY, startX, arr[startY][startX]));
+        for (int i = 0; i < distances.length; i++) {
+            for (int j = 0; j < distances[i].length; j++) {
+                distances[i][j] = Integer.MAX_VALUE;
+            }
+        }
 
-				// 위로
-				if (tmp.y - 1 >= 0 && dp[tmp.y][tmp.x] + arr[tmp.y - 1][tmp.x] < dp[tmp.y - 1][tmp.x]) {
-					dp[tmp.y - 1][tmp.x] = dp[tmp.y][tmp.x] + arr[tmp.y - 1][tmp.x];
-					queue.add(new Point(tmp.y - 1, tmp.x));
-				}
-				// 아래로
-				if (tmp.y + 1 < arr.length && dp[tmp.y][tmp.x] + arr[tmp.y + 1][tmp.x] < dp[tmp.y + 1][tmp.x]) {
-					dp[tmp.y + 1][tmp.x] = dp[tmp.y][tmp.x] + arr[tmp.y + 1][tmp.x];
-					queue.add(new Point(tmp.y + 1, tmp.x));
-				}
-				// 왼쪽
-				if (tmp.x - 1 >= 0 && dp[tmp.y][tmp.x] + arr[tmp.y][tmp.x - 1] < dp[tmp.y][tmp.x - 1]) {
-					dp[tmp.y][tmp.x - 1] = dp[tmp.y][tmp.x] + arr[tmp.y][tmp.x - 1];
-					queue.add(new Point(tmp.y, tmp.x - 1));
-				}
-				// 아래로
-				if (tmp.x + 1 < arr[0].length && dp[tmp.y][tmp.x] + arr[tmp.y][tmp.x + 1] < dp[tmp.y][tmp.x + 1]) {
-					dp[tmp.y][tmp.x + 1] = dp[tmp.y][tmp.x] + arr[tmp.y][tmp.x + 1];
-					queue.add(new Point(tmp.y, tmp.x + 1));
-				}
+        while (!pQ.isEmpty()) {
+            Vertex curVertex = pQ.remove();
 
-			}
+            if (visited[curVertex.y][curVertex.x])
+                continue;
 
-			sb.append("Problem ").append(test_case).append(": ").append(dp[N-1][N-1]).append("\n");
+            visited[curVertex.y][curVertex.x] = true;
+            distances[curVertex.y][curVertex.x] = curVertex.weight;
 
-		}
-		System.out.println(sb);
+            for (int direction = 0; direction < 4; direction++) {
+                int newY = curVertex.y + dy[direction];
+                int newX = curVertex.x + dx[direction];
 
-	}
+                if (newY < 0 || newY >= arr.length || newX < 0 || newX >= arr[0].length) {
+                    continue;
+                }
 
+                pQ.add(new Vertex(newY, newX, curVertex.weight + arr[newY][newX]));
+            }
+
+        }
+        return distances;
+    }
 }
