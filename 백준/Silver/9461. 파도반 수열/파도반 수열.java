f23af5f6f28591910a9b49ec33026e1da3cbd87b
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 13:00:46 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -7,8 +7,12 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int T;
-        T = Integer.parseInt(br.readLine());
+        // 나선에서 가장 긴 변의 길이 k
+        // P(N) 나선에 있는 정삼각형의 변의 길이
+
+        // 1 1 1 2 2 3 4 5 7 9
+
+        // N: 1 ~ 100
 
         long[] dp = new long[101];
         dp[1] = 1;
@@ -16,18 +20,14 @@ public class Main {
         dp[3] = 1;
         dp[4] = 2;
         dp[5] = 2;
-        for (int i = 6; i <= 100; i++) {
+        for (int i = 6; i <= 100; i++)
             dp[i] = dp[i - 1] + dp[i - 5];
-        }
 
-
+        int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-
             sb.append(dp[N]).append("\n");
-
         }
-
         System.out.println(sb);
 
     }
