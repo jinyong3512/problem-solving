81f690fa6c09e01ebf0ca530ec9de77e0b0339bf
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 16:43:23 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 148 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -7,42 +7,30 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        // 증가하는 부분 수열 중에서 합이 가장 큰 것
+        int N = Integer.parseInt(br.readLine());
 
-        // N 10^3
-        // number 10^3
-
-        int N;
-        int[] A;
-
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
-
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        //////////////////////////////////////////////////////////
+        //////////////////////////////////////////////
 
-        // i를 무조건 선택했을 때 증가하는 부분 수열중 합의 최대
         int[] dp = new int[N];
 
-        dp[0] = A[0];
-        for (int i = 1; i < N; i++) {
-            dp[i] = A[i];
+        for (int i = 0; i < N; i++) {
+            dp[i] = arr[i];
+
             for (int j = 0; j < i; j++) {
-                // j보다 크다
-                if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
-                }
+                if( arr[i] > arr[j])
+                    dp[i] = Math.max(dp[i],dp[j]+arr[i]);
             }
         }
 
         int answer = 0;
-
-        for(int i = 0 ; i < N ; i++)
+        for(int i =0 ; i < N ; i++)
             answer = Math.max(answer,dp[i]);
-
         System.out.println(answer);
 
     }
