cf4fd1704e44359f25d865641262c9c29dd100b6
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 16:51:48 +0900

    [Silver II] Title: 제곱수의 합, Time: 152 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,61 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static ArrayList<Integer> numbers = new ArrayList<Integer>();
-	static int[] dp = new int[100001];
-	
-	
-	public static void main(String[] args) throws IOException{
-		
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		int n = Integer.valueOf(br.readLine());
-		
-		ans(n);
-		bw.write(dp[n] + "\n");
-		
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	public static void ans(int n) {
-		
-		
-		dp[1] = 1;
-		
-		for(int i=2; i<=n; i++) {
-			int min=100001;
-			
-			for(int j=1; j<=(int)i/2; j++) {
-				
-				if(j*j == i) { // i가 어떤수의 제곱인 경우는 dp가 1이다
-					min = 1;
-					break;
-				}
-				else{
-					min = Math.min(min, dp[j]+dp[i-j]);
-				}
-				
-			}
-			dp[i] = min;
-			//System.out.println("dp["+i + "] == " + min);
-		}
-		
-		return;
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		
+        int N = Integer.parseInt(br.readLine());
+
+        //////////////////////////////////////////////
+
+        int[] dp = new int[N+1];
+        for(int i = 1; i <= N ; i++)
+            dp[i] = Integer.MAX_VALUE;
+
+        dp[0] = 0;
+
+        for(int i = 1 ; i <= N; i++){
+
+            for(int j = 1; j * j <= i ; j++){
+                dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
+
+        System.out.println(dp[N]);
+
+    }
 }
