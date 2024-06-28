d6f6c52c50535a34311b504b1dff27a6265d3766
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 13:50:15 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -18,17 +18,29 @@ public class Main {
 
         /////////////////////////////////////////////////
 
+        // 바텀업 역순회
+
+//        int[] dp = new int[N + 2];
+//
+//        for (int i = N; i >= 1; i--) {
+//            if (i + arr[i][0] <= N + 1) {
+//                dp[i] = Math.max(dp[i + 1], arr[i][1] + dp[i + arr[i][0]]);
+//            } else {
+//                dp[i] = dp[i + 1];
+//            }
+//        }
+//
+//        System.out.println(dp[1]);
+
         int[] dp = new int[N + 2];
+        for (int i = 1; i <= N; i++) {
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
 
-        for (int i = N; i >= 1; i--) {
-            if (i + arr[i][0] <= N + 1) {
-                dp[i] = Math.max(dp[i + 1], arr[i][1] + dp[i + arr[i][0]]);
-            } else {
-                dp[i] = dp[i + 1];
-            }
+            if (i + arr[i][0] <= N + 1)
+                dp[i+arr[i][0]] = Math.max(dp[i+arr[i][0]], dp[i] + arr[i][1]);
+
         }
-
-        System.out.println(dp[1]);
+        System.out.println(dp[N+1]);
 
     }
 }
