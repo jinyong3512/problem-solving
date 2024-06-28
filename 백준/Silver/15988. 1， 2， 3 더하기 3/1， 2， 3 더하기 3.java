d92e68c4261399a7589bf8a2a7036a12f2e6ff87
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 15:06:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 152 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -7,43 +7,20 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-
-        // n은 양수 1,000,000 보다 작거나 같다. 10^6
-
-        long[] dp = new long[1000001];
-        dp[0] = 0;
+        int[] dp = new int[1000001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-
-        ////////////////////////////////////////////////////////////////////////
-
-        int T;
-
-        T = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < T; t++) {
-            int N;
-
-            N = Integer.parseInt(br.readLine());
-
-            // 1
-            // 1+1 2
-            // 1+2 , 1+1+1 , 2+1 , 3
-            // 1+3 , 1+1+2, 2+2, 1+2+1 1+1+1+1 2+1+1 3+1
-
-            if (dp[N] == 0) {
-                for (int i = 4; i <= N; i++) {
-                    dp[i] = (((dp[i - 1] + dp[i - 2]) % 1000000009) + dp[i - 3]) % 1000000009;
-                }
-            }
-
-
-            sb.append(dp[N]).append("\n");
-
-
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = ((dp[i - 1] + dp[i - 2]) % 1000000009 + dp[i - 3]) % 1000000009;
         }
 
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
         System.out.println(sb);
 
 
