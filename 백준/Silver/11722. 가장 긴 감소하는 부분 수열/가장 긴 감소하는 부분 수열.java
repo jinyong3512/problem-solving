4be23eb517879973bfbaf9583e5aa885275bb92f
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-06-28 21:08:31 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,42 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int N;
-        int[] arr;
+        // dp[i]
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N ; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        /////////////////////////////////////////////
+        ////////////////////////////////////////////
 
         int[] dp = new int[N];
 
-        for(int i =0 ; i < N ; i ++){
+        for (int i = 0; i < N; i++) {
             dp[i] = 1;
 
-            for(int j =0  ; j < i ; j++){
-                if(arr[j] > arr[i]){
+            for (int j = 0; j < i; j++) {
+                if(arr[i] < arr[j])
                     dp[i] = Math.max(dp[i],dp[j]+1);
-                }
             }
         }
 
-        int answer = 1;
-        for(int i =0 ; i < N ; i++){
-            answer = Math.max(answer,dp[i]);
-        }
+        int answer = 0 ;
+        for(int i =0 ; i < N ; i ++)
+            answer = Math.max(answer, dp[i]);
         System.out.println(answer);
-
-
     }
 }
