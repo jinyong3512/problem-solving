51ab68142d56d6ed84887ea1a2884342ae4b1b52
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-07-05 16:26:09 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 268 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -2,90 +2,74 @@ import java.io.*;
 import java.util.*;
 
 class Data {
-    int number;
+    int position;
     int depth;
 
-    Data(int number, int depth) {
-        this.number = number;
+    Data(int position, int depth) {
+        this.position = position;
         this.depth = depth;
     }
 }
 
 public class Main {
-
-    // N이 10만 10^6
-    // X-1 or X+1 or 2*X
-
-    // 어떻게 이동해야 하는지
-    // 그러면 해당 number는 누가 와서 만든건지 알아야함 2는 1이 만들었다.
-    // prev를 쓰자!
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int N, K;
-
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        ////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////
 
+        Queue<Data> queue = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
         int[] prev = new int[100001];
 
-        boolean[] visited = new boolean[100001];
-        Queue<Data> queue = new LinkedList<>();
-
-        visited[N] = true;
         queue.add(new Data(N, 0));
+        visited[N] = true;
 
         while (!queue.isEmpty()) {
-            Data tmp = queue.remove();
+            Data curData = queue.remove();
 
-            if (tmp.number == K) {
-                sb.append(tmp.depth).append("\n");
+            if (curData.position == K) {
+                sb.append(curData.depth).append("\n");
                 break;
             }
 
-            // X-1
-            if (tmp.number - 1 >= 0 && !visited[tmp.number - 1]) {
-                visited[tmp.number - 1] = true;
-                prev[tmp.number - 1] = tmp.number;
-                queue.add(new Data(tmp.number - 1, tmp.depth + 1));
-            }
-            if (tmp.number + 1 <= 100000 && !visited[tmp.number + 1]) {
-                visited[tmp.number + 1] = true;
-                prev[tmp.number + 1] = tmp.number;
-                queue.add(new Data(tmp.number + 1, tmp.depth + 1));
-            }
-            if (tmp.number * 2 <= 100000 && !visited[tmp.number * 2]) {
-                visited[tmp.number * 2] = true;
-                prev[tmp.number * 2] = tmp.number;
-                queue.add(new Data(tmp.number * 2, tmp.depth + 1));
-            }
+            for (int direction = 0; direction < 3; direction++) {
 
+                int newPosition;
+
+                if (direction == 0)
+                    newPosition = curData.position - 1;
+                else if (direction == 1)
+                    newPosition = curData.position + 1;
+                else
+                    newPosition = curData.position * 2;
+
+                if (newPosition < 0 || newPosition > 100000)
+                    continue;
+
+                if (visited[newPosition])
+                    continue;
+
+                visited[newPosition] = true;
+                prev[newPosition] = curData.position;
+                queue.add(new Data(newPosition, curData.depth + 1));
+            }
         }
-
-        recursion(sb, prev, N, K);
-
+        recursion(K, N, prev, sb);
+        sb.append(K).append(" ");
         System.out.println(sb);
-
     }
 
-    public static void recursion(StringBuilder sb, int[] prev, int N, int cur) {
-        // K를 넣어서 맨 뒤를 구하는거야
-
-        if (cur == N) {
-
-            sb.append(cur).append(" ");
-
+    public static void recursion(int curPosition, int N, int[] prev, StringBuilder sb) {
+        if(curPosition == N)
             return;
-        }
 
-        recursion(sb, prev, N, prev[cur]);
-
-        sb.append(cur).append(" ");
+        recursion(prev[curPosition], N, prev, sb);
+        sb.append(prev[curPosition]).append(" ");
     }
 }
