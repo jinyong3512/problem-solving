267a827426f0e31f33f0f7f86238e66e4491b281
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-07-06 16:08:00 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,20 @@
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+    
+FROM
+    ANIMAL_INS AS A
+    
+WHERE
+    A.NAME = 'Lucy'
+    OR
+    A.NAME = 'Ella'
+    OR
+    A.NAME = 'Pickle'
+    OR
+    A.NAME = 'Rogan'
+    OR
+    A.NAME = 'Sabrina'
+    OR
+    A.NAME = 'Mitty'
