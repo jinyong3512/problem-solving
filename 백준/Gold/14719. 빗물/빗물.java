0ce16d4945f14bf166305dd804d5c323ab306278
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-07-09 00:13:09 +0900

    [Gold V] Title: 빗물, Time: 136 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -2,60 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		int H, W;
-		boolean[][] arr;
+        // 24:02 ~ 24:12 
+        st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
 
-		st = new StringTokenizer(br.readLine());
-		H = Integer.parseInt(st.nextToken());
-		W = Integer.parseInt(st.nextToken());
-		arr = new boolean[H][W];
+        boolean[][] arr = new boolean[H][W];
 
-		st = new StringTokenizer(br.readLine());
-		for (int j = 0; j < W; j++) {
-			int tmp_height = Integer.parseInt(st.nextToken());
-			for (int i = 0; i < tmp_height; i++) {
-				arr[i][j] = true;
-			}
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < W; j++) {
+            int height = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < height; i++) {
+                arr[H - 1 - i][j] = true;
+            }
+        }
 
-		///////////////////////////////////////////////
+//        for(int i = 0 ; i < H ; i++){
+//            for(int j = 0 ; j < W; j ++){
+//                if(arr[i][j])
+//                    System.out.print("1 ");
+//                else
+//                    System.out.print("0 ");
+//            }
+//            System.out.println();
+//        }
 
-		int answer = 0;
+        int answer = 0;
 
-		for (int i = 0; i < H; i++) {
-			// 맨 왼쪽 벽
-			int left = -1;
-			// 맨 오른쪽 벽
-			int right = -1;
+        for (int i = H - 1; i >= 0; i--) {
+            int left = 0;
+            int right = W - 1;
 
-			for (int j = 0; j < W; j++) {
-				if (arr[i][j]) {
-					left = j;
-					break;
-				}
-			}
+            for (; left < W; left++) {
+                if (arr[i][left])
+                    break;
+            }
 
-			for (int j = W - 1; j >= 0; j--) {
-				if (arr[i][j]) {
-					right = j;
-					break;
-				}
-			}
+            for (; right >= 0; right--) {
+                if (arr[i][right])
+                    break;
+            }
 
-			if (left != -1 && right != -1) {
-				for (int j = left + 1; j < right; j++) {
-					if (!arr[i][j])
-						answer++;
-				}
-			}
-		}
-		System.out.println(answer);
+            if (left == W)
+                continue;
+            if (left == right)
+                continue;
 
-	}
+            int count = 0;
+            for(int j = left+1 ; j < right; j++){
+                if(!arr[i][j])
+                    count++;
+            }
 
+            answer += (count);
+        }
+
+        System.out.println(answer);
+    }
 }
