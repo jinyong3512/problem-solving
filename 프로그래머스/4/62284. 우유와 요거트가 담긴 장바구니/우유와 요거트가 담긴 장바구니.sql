c7d09b604117625a45a97e3d8296a0a66d3d70cb
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-07-08 22:34:23 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -3,25 +3,20 @@ SELECT
 FROM
     (
     SELECT
-        *
+        DISTINCT(CART_ID)
     FROM
         CART_PRODUCTS
     WHERE
-        NAME = 'Milk' OR NAME = 'Yogurt'
+        NAME = 'Milk'
     ) AS A
 INNER JOIN
     (
     SELECT
-        *
+        DISTINCT(CART_ID)
     FROM
         CART_PRODUCTS
     WHERE
-        NAME = 'Milk' OR NAME = 'Yogurt'
+        NAME = 'Yogurt'
     ) AS B ON A.CART_ID = B.CART_ID
-WHERE 
-    A.NAME = 'Milk' AND B.NAME = 'Yogurt'
-GROUP BY
-    A.CART_ID
 ORDER BY
     A.CART_ID ASC
-    
