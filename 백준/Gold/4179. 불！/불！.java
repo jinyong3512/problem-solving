f5c94b6144711ff08ba669cae0d9933f4970c505
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-08-03 15:06:29 +0900

    [Gold III] Title: 불!, Time: 544 ms, Memory: 76588 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.util.*;
 
-class Point {
+class Data {
     int y, x, depth;
 
-    Point(int y, int x, int depth) {
+    Data(int y, int x, int depth) {
         this.y = y;
         this.x = x;
         this.depth = depth;
@@ -21,128 +21,131 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        // 1400 시작
-        int R, C;
-        int[][] map;
-        Point start = null;
+        // # 벽
+        // . 지나갈 수 있는 공간
+        // J 초기 위치
+        // F 불이 난 공간
+        // J는 하나만
 
         st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        map = new int[R][C];
-        for (int i = 0; i < map.length; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < map[0].length; j++) {
-                char one = line.charAt(j);
-                if (one == '#') {
-                    map[i][j] = -1;
-                } else if (one == 'F') {
-                    map[i][j] = 0;
-                } else if (one == 'J') {
-                    map[i][j] = Integer.MAX_VALUE;
-                    start = new Point(i, j, 0);
-                } else if (one == '.') {
-                    map[i][j] = Integer.MAX_VALUE;
+        char[][] arr = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < C; j++) {
+                arr[i][j] = input.charAt(j);
+            }
+        }
+
+        /////////////////////////////////////////
+
+        int[][] timeTable = calculateTimeTable(arr, R, C);
+
+        int answer = getOut(arr, R, C, timeTable);
+
+        if (answer == Integer.MAX_VALUE)
+            System.out.println("IMPOSSIBLE");
+        else
+            System.out.println(answer);
+
+
+    }
+
+    public static int[][] calculateTimeTable(char[][] arr, int R, int C) {
+
+        int[][] timeTable = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                timeTable[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        Queue<Data> queue = new LinkedList<>();
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (arr[i][j] == 'F') {
+                    queue.add(new Data(i, j, 0));
+                    timeTable[i][j] = 0;
                 }
             }
         }
 
-        /////////////////////////////////////////////////////////////////////////
-
-        spreadFire(map);
-
-//        for (int i = 0; i < map.length; i++) {
-//            for (int j = 0; j < map[0].length; j++) {
-//                System.out.print(map[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        Queue<Point> queue = new LinkedList<>();
-
-        visited[start.y][start.x] = true;
-        queue.add(start);
-
         while (!queue.isEmpty()) {
-            Point tmp = queue.remove();
-
-//            System.out.println("y = " + tmp.y + " x = "+tmp.x + " depth = " + tmp.depth);
-
-            if (tmp.y == 0 || tmp.y == map.length - 1 || tmp.x == 0 || tmp.x == map[0].length - 1) {
-                sb.append(tmp.depth + 1);
-                break;
-            }
+            Data curData = queue.remove();
 
             for (int direction = 0; direction < 4; direction++) {
-                int newY = tmp.y + dy[direction];
-                int newX = tmp.x + dx[direction];
+                int newY = curData.y + dy[direction];
+                int newX = curData.x + dx[direction];
 
-                if (newY < 0 || newY >= map.length || newX < 0 || newX >= map[0].length)
+                if (newY < 0 || newY >= R || newX < 0 || newX >= C)
                     continue;
 
-                if (map[newY][newX] == -1)
+                if (arr[newY][newX] == '#')
                     continue;
 
-                if (visited[newY][newX])
+                if (timeTable[newY][newX] != Integer.MAX_VALUE)
                     continue;
 
-                // map의 값은 불이 있기 시작한 시간
-                if (map[newY][newX] <= tmp.depth + 1)
-                    continue;
+                timeTable[newY][newX] = curData.depth + 1;
 
-                visited[newY][newX] = true;
-                queue.add(new Point(newY, newX, tmp.depth + 1));
+                queue.add(new Data(newY, newX, curData.depth + 1));
 
             }
+
         }
 
-        if (sb.length() == 0) {
-            sb.append("IMPOSSIBLE");
-        }
-
-        System.out.println(sb);
+        return timeTable;
 
     }
 
-    public static void spreadFire(int[][] map) {
+    public static int getOut(char[][] arr, int R, int C, int[][] timeTable) {
 
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[R][C];
+        Queue<Data> queue = new LinkedList<>();
 
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] == 0) {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (arr[i][j] == 'J') {
+                    queue.add(new Data(i, j, 0));
                     visited[i][j] = true;
-                    queue.add(new Point(i, j, 0));
+
+                    if (i == 0 || i == R - 1 || j == 0 || j == C - 1)
+                        return 1;
                 }
             }
         }
 
-        while (!queue.isEmpty()) {
-            Point tmp = queue.remove();
 
-            map[tmp.y][tmp.x] = tmp.depth;
+        while (!queue.isEmpty()) {
+            Data curData = queue.remove();
 
             for (int direction = 0; direction < 4; direction++) {
-                int newY = tmp.y + dy[direction];
-                int newX = tmp.x + dx[direction];
+                int newY = curData.y + dy[direction];
+                int newX = curData.x + dx[direction];
 
-                if (newY < 0 || newY >= map.length || newX < 0 || newX >= map[0].length)
+                if (arr[newY][newX] == '#')
                     continue;
 
-                if (map[newY][newX] == -1)
+                if (timeTable[newY][newX] <= curData.depth + 1)
                     continue;
+
+                if (newY == 0 || newY == R - 1 || newX == 0 || newX == C - 1)
+                    return curData.depth + 1 + 1;
 
                 if (visited[newY][newX])
                     continue;
 
                 visited[newY][newX] = true;
-                queue.add(new Point(newY, newX, tmp.depth + 1));
+                queue.add(new Data(newY, newX, curData.depth + 1));
 
             }
+
         }
 
+        return Integer.MAX_VALUE;
+
     }
+
 }
