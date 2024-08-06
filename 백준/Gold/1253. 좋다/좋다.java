a61736b79e78f048becb0d02f9ea41584e046d2f
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-08-07 01:32:17 +0900

    [Gold IV] Title: 좋다, Time: 364 ms, Memory: 18736 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -11,92 +11,83 @@ public class Main {
 
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(st.nextToken());
-        }
 
         //////////////////////////////////////////////
 
-        // 다른 두 개의 합으로 나타나면 좋다
-
-        // 수의 위치가 다르면 값이 같아도 다른 수이다
-
-        int answer = 0;
-
         Arrays.sort(arr);
 
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+
         for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                int lowerBoundIndex = lowerBound(arr, i, j);
+                int upperBoundIndex = upperBound(arr, i, j);
 
-            boolean find = false;
+                int tmpAnswer = upperBoundIndex - lowerBoundIndex;
 
-            for (int j = 0; j < N; j++) {
-                if (i == j)
-                    continue;
+//                System.out.println("value = " + (arr[i] + arr[j]) / 2 + " lowerBoundIndex = " + lowerBoundIndex + " upperBoundIndex = " + upperBoundIndex);
 
-                int findValue = arr[i] - arr[j];
+                if (lowerBoundIndex <= i && i < upperBoundIndex)
+                    tmpAnswer--;
+                if (lowerBoundIndex <= j && j < upperBoundIndex)
+                    tmpAnswer--;
 
-                int lowerboundIndex = lowerbound(arr, findValue);
-                int upperboundIndex = upperbound(arr, findValue);
-
-                if (lowerboundIndex <= i && i <= upperboundIndex - 1) {
-                    if (lowerboundIndex <= j && j <= upperboundIndex - 1) {
-                        if (upperboundIndex - lowerboundIndex > 2) {
-                            find = true;
-                        }
-                    } else {
-                        if (upperboundIndex - lowerboundIndex > 1) {
-                            find = true;
-                        }
-                    }
-                } else if (lowerboundIndex <= j && j <= upperboundIndex - 1) {
-                    if (upperboundIndex - lowerboundIndex > 1) {
-                        find = true;
-                    }
-                } else {
-                    if (upperboundIndex - lowerboundIndex > 0) {
-                        find = true;
-                    }
-                }
+                if (tmpAnswer > 0)
+                    hashMap.put(arr[i] + arr[j], true);
             }
+        }
 
-            if (find)
+        int answer = 0;
+        for (int value : arr) {
+            if (hashMap.containsKey(value))
                 answer++;
-
         }
+
         System.out.println(answer);
+
     }
 
-    public static int lowerbound(int[] arr, int findValue) {
-        int leftIndex = 0;
-        int rightIndex = arr.length - 1;
+    public static int lowerBound(int[] arr, int left, int right) {
+        int value = arr[left] + arr[right];
 
-        while (leftIndex <= rightIndex) {
-            int mid = (leftIndex + rightIndex) / 2;
+        int low = 0;
+        int high = arr.length - 1;
 
-            if (arr[mid] >= findValue) {
-                rightIndex = mid - 1;
-            } else {
-                leftIndex = mid + 1;
-            }
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int curValue = arr[mid];
+
+            if (curValue < value)
+                low = mid + 1;
+            else if (curValue == value)
+                high = mid - 1;
+            else
+                high = mid - 1;
         }
 
-        return leftIndex;
+        return low;
     }
 
-    public static int upperbound(int[] arr, int findValue) {
-        int leftIndex = 0;
-        int rightIndex = arr.length - 1;
+    public static int upperBound(int[] arr, int left, int right) {
+        int value = arr[left] + arr[right];
 
-        while (leftIndex <= rightIndex) {
-            int mid = (leftIndex + rightIndex) / 2;
+        int low = 0;
+        int high = arr.length - 1;
 
-            if (arr[mid] > findValue) {
-                rightIndex = mid - 1;
-            } else {
-                leftIndex = mid + 1;
-            }
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int curValue = arr[mid];
+
+            if (curValue < value)
+                low = mid + 1;
+            else if (curValue == value)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
 
-        return leftIndex;
+        return low;
     }
 }
