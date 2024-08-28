1ad3e8f9c7c7ddb39b8ad36526a553dde708ae9f
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-08-28 18:45:31 +0900

    [Gold V] Title: 센서, Time: 156 ms, Memory: 16184 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,47 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-class Data {
-    int number;
-    boolean tower;
-
-    Data(int number, boolean tower) {
-        this.number = number;
-        this.tower = tower;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
+        // 1 3 6 6 7 9
+
+        // 123 -> 2
+        // 6789 -> 3
+
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
 
-        int[] sensors = new int[N];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            sensors[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        //////////////////////////////////////////////////
+        //////////////////////////////////////////////
 
-        Arrays.sort(sensors);
+        Arrays.sort(arr);
+
+        // N개 중에 K개를 조합으로 골라서 최솟값을 구할 수 있지만
+        // 흠,,,,,,,,,,,,,,,,,,,,,,,
+        // 모르겠다 ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ
+
+        // 1 3 6 6 7 9
+        // 2 3 0 1 2
 
         int[] gaps = new int[N-1];
-
-        for(int i = 0 ; i < sensors.length-1 ; i++){
-            gaps[i] = sensors[i+1] - sensors[i];
-        }
+        for(int i =0 ; i < N-1 ; i++)
+            gaps[i] = arr[i+1]-arr[i];
 
         Arrays.sort(gaps);
 
         int answer = 0;
-
-        for(int i = 0 ; i < gaps.length - K + 1; i++){
+        for(int i = 0 ; i < gaps.length - K + 1 ; i++)
             answer += gaps[i];
-        }
 
         System.out.println(answer);
 
