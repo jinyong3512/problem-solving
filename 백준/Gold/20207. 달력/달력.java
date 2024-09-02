1e608866cff090497fdfb6a246f5c244b3d49e9a
김진용 <88269663+jinyong3512@users.noreply.github.com>
2024-09-03 00:36:47 +0900

    [Gold V] Title: 달력, Time: 116 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -7,6 +7,13 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
+//        solve1(br, sb, null);
+        solve2(br, sb, null);
+
+
+    }
+
+    public static void solve1(BufferedReader br, StringBuilder sb, StringTokenizer st) throws IOException {
         int N = Integer.parseInt(br.readLine());
 
         int[] diff = new int[367];
@@ -51,6 +58,45 @@ public class Main {
         }
 
         System.out.println(answer);
+    }
 
+    public static void solve2(BufferedReader br, StringBuilder sb, StringTokenizer st) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dates = new int[366];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            for (int j = start; j <= end; j++)
+                dates[j]++;
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= 365; i++) {
+            if (dates[i] == 0)
+                continue;
+
+            int maxValue = 0;
+            int count = 0;
+            while (true) {
+                if (i > 365)
+                    break;
+
+                if (dates[i] == 0)
+                    break;
+
+                count++;
+                maxValue = Math.max(maxValue, dates[i]);
+                i++;
+            }
+
+            answer += maxValue * count;
+
+        }
+
+        System.out.println(answer);
     }
 }
