4dcfd3114a802f438419e6e89467568f2cbd6a4e
김진용 <88269663+jinyong3512@users.noreply.github.com>
2025-10-17 21:27:36 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 5.19 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -2,50 +2,37 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
+    public boolean check (int[] mats, String[][] park, int i, int j, int lastTrySize) {
+        for (int y = 0; y < lastTrySize; y++) {
+            for (int x = 0; x < lastTrySize; x++) {
+                int newY = i + y;
+                int newX = j + x;
+                if (newY >= park.length || newX >= park[0].length)
+                    return false;
+                if (!park[newY][newX].equals("-1"))
+                    return false;
+            }
+        }
+        return true;
+    }
     public int solution(int[] mats, String[][] park) {    
-        int maxLength = -1;
+        int lastTrySize = 1;
         
         for (int i = 0; i < park.length; i++) {
             for (int j = 0; j < park[0].length; j++) {
-                if (!park[i][j].equals("-1")) {
-                    // System.out.printf("%d ", maxLength);
-                    continue;
-                }
+                if (!park[i][j].equals("-1")) 
+                    continue;                
                 
-                boolean success = true;
-                while (success) {
-                    for (int y = 0; y < maxLength + 1; y++) {
-                        if (i + y >= park.length) {
-                            success = false;
-                            break;
-                        }                        
-                        for (int x = 0; x < maxLength + 1; x++) {
-                            if (j + x >= park[0].length) {
-                                success = false;
-                                break;
-                            }
-                            if (!park[i + y][j + x].equals("-1")){
-                                success = false;
-                                break;
-                            }
-                        }
-                        if (!success)
-                            break;
-                    }
-                    if (success)
-                        maxLength++;
-                }
-                
-                // System.out.printf("%d ", maxLength);
+                while (check(mats, park, i, j, lastTrySize))
+                    lastTrySize++;                
             }
-            // System.out.println();
         }
         
         int answer = -1;
         
-        for (int value : mats) {
-            if (value <= maxLength)
-                answer = Math.max(answer, value);
+        for (int mat : mats) {
+            if (mat <= lastTrySize-1)
+                answer = Math.max(answer, mat);
         }
         
         return answer;
