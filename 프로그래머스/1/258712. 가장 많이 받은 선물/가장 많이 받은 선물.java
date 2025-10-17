c66552acee3b1262c1fb72ee98872cdd0eaf202f
김진용 <88269663+jinyong3512@users.noreply.github.com>
2025-10-17 22:05:07 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 18.03 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -3,11 +3,12 @@ import java.io.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        HashMap<String,Integer> hashMap = new HashMap<>();
+        HashMap<String,Integer> hashMap = new HashMap<>();        
+        int[][] map = new int[friends.length][friends.length];
+        int[] points = new int[friends.length];
+                
         for (int i = 0; i < friends.length; i++)
             hashMap.put(friends[i], i);
-        
-        int[][] map = new int[friends.length][friends.length];
         
         for (String gift : gifts) {
             StringTokenizer st = new StringTokenizer(gift);
@@ -16,15 +17,9 @@ class Solution {
             
             map[hashMap.get(A)][hashMap.get(B)]++;
             map[hashMap.get(B)][hashMap.get(A)]--;
-        }
-        
-        int[] giftPoints = new int[friends.length];
-        for (int i = 0; i < map.length; i++) {
-            int tmpGiftPoint = 0;
-            for (int j = 0 ; j < map[0].length; j++) {
-                tmpGiftPoint += map[i][j];
-            }
-            giftPoints[i] = tmpGiftPoint;
+            
+            points[hashMap.get(A)]++;
+            points[hashMap.get(B)]--;
         }
         
         int answer = 0;
@@ -34,7 +29,7 @@ class Solution {
             for (int j = 0; j < map[0].length; j++) {
                 if (map[i][j] > map[j][i]) {
                     tmpAnswer++;
-                } else if (map[i][j] == map[j][i] && giftPoints[i] > giftPoints[j]) {
+                } else if (map[i][j] == map[j][i] && points[i] > points[j]) {
                     tmpAnswer++;
                 }
             }
