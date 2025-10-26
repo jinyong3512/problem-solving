a45045b94f9b7b82652c4c824f9a77f186a78b4e
김진용 <88269663+jinyong3512@users.noreply.github.com>
2025-10-26 12:57:09 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,38 +1,21 @@
-import java.util.*;
-
-class Server {
-    int endTime, count;
-    Server (int endTime, int count) {
-        this.endTime = endTime;
-        this.count = count;
-    }
-}
-
 class Solution {
     public int solution(int[] players, int m, int k) {
         
-        int n = 1;
+        int[] activeHistory = new int[24];
+        int active = 0;
         int x = 0;
         
-        Queue<Server> queue = new LinkedList<>();                        
-        
         for (int i = 0; i < players.length; i++) {
+            if (i - k >= 0) 
+                active -= activeHistory[i - k];
             
-            // 증설했던 것 빼주기
-            if (!queue.isEmpty() && queue.peek().endTime == i) {                
-                n -= queue.remove().count;
-            }
-            
-            // 증설하기
-            if (players[i] >= n * m) {
-                int count = players[i] / m + 1 - n;
-                
-                queue.add(new Server(i + k, count));
-                n += count;
-                x += count;
+            if (players[i] / m > active) {
+                int needMoreServer = players[i] / m - active;
+                activeHistory[i] = needMoreServer;
+                active += needMoreServer;  
+                x += needMoreServer;
             }
         }
-        
         
         return x;
     }
