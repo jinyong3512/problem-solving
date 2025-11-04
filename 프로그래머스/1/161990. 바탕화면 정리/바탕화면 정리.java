4dedcf11517f3fa07f384fbf2c5a7f211bd9acb9
김진용 <88269663+jinyong3512@users.noreply.github.com>
2025-11-04 21:54:40 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,64 +2,22 @@ class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = {};
         
-        int luy = -1;
-        int lux = -1;        
-        int rdy = -1;
-        int rdx = -1;
+        int minY = Integer.MAX_VALUE;
+        int minX = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        int maxX = Integer.MIN_VALUE;
         
-        for (int i = 0; i < wallpaper.length; i++) {
-            boolean find = false;
+        for (int i = 0; i < wallpaper.length; i++) {            
             for (int j = 0; j < wallpaper[i].length(); j++) {
                 if (wallpaper[i].charAt(j) == '#') {
-                    find = true;
+                    minY = Math.min(minY, i);
+                    minX = Math.min(minX, j);
+                    maxY = Math.max(maxY, i);
+                    maxX = Math.max(maxX, j);
                 }
             }
-            if (find){
-                lux = i;
-                break;
-            }
-        }
+        }            
         
-        for (int i = wallpaper.length - 1; i >= 0; i--) {
-            boolean find = false;
-            for (int j = 0; j < wallpaper[i].length(); j++) {
-                if (wallpaper[i].charAt(j) == '#') {
-                    find = true;
-                }
-            }
-            if (find) {
-                rdx = i;
-                break;
-            }
-        }    
-        
-        for (int j = 0; j < wallpaper[0].length(); j++) {
-            boolean find = false;
-            for (int i = 0; i < wallpaper.length; i++) {
-                if (wallpaper[i].charAt(j) == '#') {
-                    find = true;
-                }
-            }
-            if (find) {
-                luy = j;
-                break;
-            }
-        }
-        
-        for (int j = wallpaper[0].length() - 1; j >= 0; j--) {
-            boolean find = false;
-            for (int i = 0; i < wallpaper.length; i++) {
-                if (wallpaper[i].charAt(j) == '#') {
-                    find = true;
-                }
-            }
-            if (find) {
-                rdy = j;
-                break;
-            }
-        }        
-        
-        
-        return new int[]{lux, luy, rdx + 1, rdy + 1};
+        return new int[]{minY, minX, maxY + 1, maxX + 1};
     }
 }
