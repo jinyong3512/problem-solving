24bb39ed8520d7b9d891fd7996a375ee7374d2d1
김진용 <88269663+jinyong3512@users.noreply.github.com>
2025-11-05 21:51:15 +0900

    [level 1] Title: 덧칠하기, Time: 1.39 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,15 +2,12 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         
+        int lastLocation = 0;
         for (int i = 0; i < section.length; i++) {
-            answer++;
-            int lastLocation = section[i] + m - 1;
-            while (true) {
-                i++;
-                if (i >= section.length || section[i] > lastLocation)
-                    break;
+            if (lastLocation < section[i]){
+                answer++;
+                lastLocation = section[i] + m - 1;
             }
-            i--;
         }        
         return answer;
     }
