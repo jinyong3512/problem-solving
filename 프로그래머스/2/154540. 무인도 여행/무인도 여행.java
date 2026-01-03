74a677d895cf7c55d84e9cec846ee3c1bb19f936
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-01-04 02:52:04 +0900

    [level 2] Title: 무인도 여행, Time: 7.10 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,10 +1,8 @@
 import java.util.*;
-import java.io.*;
 
 class Point {
     int y, x;
-    
-    Point(int y, int x) {
+    Point (int y, int x) {
         this.y = y;
         this.x = x;
     }
@@ -17,68 +15,65 @@ class Solution {
     
     public int[] solution(String[] maps) {
         
-        ArrayList<Integer> answer = new ArrayList<>();
-        function1(maps, answer);
-        
-        Collections.sort(answer);
-        
-        if (answer.size() == 0) {
-            return new int[]{-1};
-        } else {        
-            int[] answer2 = new int[answer.size()];
-            for (int i = 0; i < answer.size(); i++) {
-                answer2[i] = answer.get(i);
-            }
-
-            return answer2;
-        }
-    }
-    
-    public void function1(String[] maps, ArrayList<Integer> answer) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
         
         boolean[][] visited = new boolean[maps.length][maps[0].length()];
         
         for (int i = 0; i < maps.length; i++) {
             for (int j = 0; j < maps[0].length(); j++) {
-                
                 if (visited[i][j])
                     continue;
                 else if (maps[i].charAt(j) == 'X')
                     continue;
-                else {
-                    visited[i][j] = true;
-                    
-                    Queue<Point> queue = new LinkedList<>();
-                    queue.add(new Point(i, j));
-                    
-                    int count = maps[i].charAt(j) - '0';
-                                        
-                    while(!queue.isEmpty()) {
-                        Point curPoint = queue.remove();
-
-                        for(int direction = 0; direction < 4; direction++) {
-                            int newY = curPoint.y + dy[direction];
-                            int newX = curPoint.x + dx[direction];
-
-                            if(newY < 0 || newY >= maps.length || newX < 0 || newX >= maps[0].length())
-                                continue;
-                            
-                            if(visited[newY][newX])
-                                continue;
-                            
-                            if(maps[newY].charAt(newX) == 'X')
-                                continue;
-                            
-                            visited[newY][newX] = true;
-                            queue.add(new Point(newY, newX));
-                            count += maps[newY].charAt(newX) - '0';
-                        }
-                    }
-                    
-                    answer.add(count);
-                    
+                else {                    
+                    arrayList.add(bfs(maps, visited, i, j));
                 }
             }
         }
+        
+        Collections.sort(arrayList);
+        
+        if (arrayList.size() == 0) {
+            return new int[]{-1};
+        } else {
+            int[] answer = new int[arrayList.size()];
+            for (int i = 0; i < arrayList.size(); i++)
+                answer[i] = arrayList.get(i);
+            return answer;
+        }
+        
+    }
+    
+    public int bfs(String[] maps, boolean[][] visited, int i, int j) {
+        int count = maps[i].charAt(j) - '0';
+        
+        visited[i][j] = true;
+        
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(i, j));
+        
+        while (!queue.isEmpty()) {
+            
+            Point curPoint = queue.remove();
+            
+            for (int direction = 0; direction < 4; direction++) {
+                int newY = curPoint.y + dy[direction];
+                int newX = curPoint.x + dx[direction];            
+                
+                if (newY < 0 || newY >= maps.length || newX < 0 || newX >= maps[0].length())
+                    continue;
+                
+                if (visited[newY][newX])
+                    continue;
+                
+                if (maps[newY].charAt(newX) == 'X')
+                    continue;
+                
+                visited[newY][newX] = true;
+                count += maps[newY].charAt(newX) - '0';
+                queue.add(new Point(newY, newX));
+            }
+        }
+        return count;
     }
 }
