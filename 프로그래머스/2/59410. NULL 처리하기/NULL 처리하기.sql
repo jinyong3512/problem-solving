34a29003316f0b79e22ef179e05d5956ed46fb72
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-01-07 01:52:38 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,9 @@
--- 코드를 입력하세요
-SELECT
-    ANIMAL_TYPE,
-    IFNULL(NAME,'No name'),
-    SEX_UPON_INTAKE
-FROM
-    ANIMAL_INS
-ORDER BY
-    ANIMAL_ID ASC;
+-- 동물의 생물 종, 이름, 성별 및 중성화 여부
+SELECT ANIMAL_TYPE
+     , CASE 
+            WHEN NAME IS NULL THEN 'No name'
+            ELSE NAME
+       END
+     , SEX_UPON_INTAKE
+  FROM ANIMAL_INS
+ORDER BY ANIMAL_ID  
--- 프로그래머스/2/59410. NULL 처리하기/README.md
+++ 프로그래머스/2/59410. NULL 처리하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] NULL 처리하기 - 59410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59410) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59410?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 05일 19:55:38
+2026년 01월 07일 01:50:30
 
 ### 문제 설명
 
