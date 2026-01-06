62a33672bb87ac01fdbd6a519a23d5671392bd8e
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-01-07 03:04:04 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 그룹별 조건에 맞는 식당 목록 출력하기 - 131124 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131124) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131124?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 09일 01:06:39
+2026년 01월 07일 03:01:56
 
 ### 문제 설명
 
--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,32 +1,20 @@
-#  회원 이름, 리뷰 텍스트, 리뷰 작성일
-SELECT
-    X.MEMBER_NAME,
-    Y.REVIEW_TEXT,
-    DATE_FORMAT(Y.REVIEW_DATE,'%Y-%m-%d') AS REVIEW_DATE
-FROM
-    MEMBER_PROFILE AS X
-INNER JOIN
-    (
-    SELECT
-        A.MEMBER_ID,
-        A.REVIEW_TEXT,
-        A.REVIEW_DATE
-    FROM
-        REST_REVIEW AS A
-    INNER JOIN
-        (
-        SELECT
-            MEMBER_ID
-        FROM
-            REST_REVIEW
-        GROUP BY
-            MEMBER_ID
-        ORDER BY
-            COUNT(MEMBER_ID) DESC
-        LIMIT 
-            1
-        ) AS B ON A.MEMBER_ID = B.MEMBER_ID
-    ) AS Y ON X.MEMBER_ID = Y.MEMBER_ID
-ORDER BY
-    Y.REVIEW_DATE ASC,
-    Y.REVIEW_TEXT ASC
+SELECT A.MEMBER_NAME
+     , B.REVIEW_TEXT
+     , TO_CHAR(B.REVIEW_DATE, 'YYYY-MM-DD')
+  FROM MEMBER_PROFILE A
+       RIGHT OUTER JOIN REST_REVIEW B
+       ON A.MEMBER_ID = B.MEMBER_ID  
+ WHERE B.MEMBER_ID IN (
+    SELECT MEMBER_ID
+      FROM REST_REVIEW
+    GROUP BY MEMBER_ID
+    HAVING COUNT(*) = (
+        SELECT MAX(CNT)
+          FROM (
+                SELECT COUNT(*) AS CNT
+                  FROM REST_REVIEW
+                GROUP BY MEMBER_ID
+               )
+    )
+)
+ORDER BY REVIEW_DATE, REVIEW_TEXT
