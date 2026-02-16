0bfa8328ccfdd313ec3e9e03c7abbf096c0a9f5d
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-02-16 18:00:10 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.14 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -4,30 +4,49 @@ class Solution {
     public int solution(int a, int b, int c, int d) {
         // int answer = 0;
         
-        int[] numbers = new int[4];
-        numbers[0] = a;
-        numbers[1] = b;
-        numbers[2] = c;
-        numbers[3] = d;
-        
+        int[] numbers = new int[]{a, b, c, d};
         Arrays.sort(numbers);
+        a = numbers[0];
+        b = numbers[1];
+        c = numbers[2];
+        d = numbers[3];
         
-        if (numbers[0] == numbers[3]) {
-            return 1111 * numbers[0];
-        } else if (numbers[0] == numbers[2] && numbers[2] != numbers[3]) {
-            return (int)Math.pow(10 * numbers[0] + numbers[3], 2);
-        } else if (numbers[1] == numbers[3] && numbers[0] != numbers[1]) {
-            return (int)Math.pow(10 * numbers[1] + numbers[0], 2);
-        } else if (numbers[0] == numbers[1] && numbers[2] == numbers[3] && numbers[1] != numbers[2]) {
-            return (numbers[1] + numbers[2]) * Math.abs(numbers[1] - numbers[2]);
-        } else if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] == numbers[3]) {
-            return numbers[0] * numbers[1];
-        } else if (numbers[0] != numbers[1] && numbers[2] != numbers[3] && numbers[1] == numbers[2]) {
-            return numbers[0] * numbers[3];
-        } else if (numbers[1] != numbers[2] && numbers[2] != numbers[3] && numbers[0] == numbers[1]) {
-            return numbers[2] * numbers[3];
+        if (a == b) {
+            if (b == c) {
+                if (c == d) {
+                    // abcd 일치
+                    return 1111 * a;
+                } else {
+                    // abc 일치
+                    return (int)Math.pow(10 * a + d, 2);
+                }                
+            } else {
+                if (c == d) {
+                    // ab 일치 cd 일치 
+                    return (a + c) * (int)Math.abs(a - c);
+                } else {
+                    // ab 일치
+                    return c * d;
+                }
+            }
         } else {
-            return numbers[0];
+            if (b == c) {
+                if (c == d) {
+                    // bcd 일치
+                    return (int)Math.pow(10 * b + a, 2);
+                } else {
+                    // bc 일치
+                    return a * d;
+                }                
+            } else {
+                if (c == d) {
+                    // cd 일치
+                    return a * b;
+                } else {
+                    // 다 불일치
+                    return a;
+                }
+            }
         }
     }
 }
