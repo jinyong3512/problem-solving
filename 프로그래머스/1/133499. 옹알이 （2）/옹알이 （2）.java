83b8d6ad867cbaeabf10ddf7fa9a99cea4eee88d
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-02-18 14:26:45 +0900

    [level 1] Title: 옹알이 (2), Time: 1.42 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,97 +3,24 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < babbling.length; i++) {
-            if (isCan(babbling[i]))
+            if (babbling[i].contains("ayaaya")
+             || babbling[i].contains("yeye")
+             || babbling[i].contains("woowoo")
+             || babbling[i].contains("mama")) {
+                continue;
+            }
+            
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            
+            babbling[i] = babbling[i].replace(" ", "");
+            
+            if (babbling[i].length() == 0)
                 answer++;
         }
         
-        
         return answer;
-    }
-    
-    public static boolean isCan(String babbling) {
-        
-        String lastWord = "";
-        String curWord = "";
-        
-        for (int i = 0; i < babbling.length(); i++) {
-            
-            // a y e w o m
-            
-            if (babbling.charAt(i) == 'a') {
-                
-                if (curWord.isEmpty()) {
-                    curWord += 'a';
-                } else if (curWord.equals("ay") || curWord.equals("m")) {
-                    curWord += 'a';
-                    if (lastWord.equals(curWord))
-                        return false;
-                    else {
-                        lastWord = curWord;
-                        curWord = "";
-                    }
-                } else {
-                    return false;
-                }
-                
-            } else if (babbling.charAt(i) == 'y') {
-                
-                if (curWord.isEmpty()) {
-                    curWord += 'y';
-                } else if (curWord.equals("a")) {
-                    curWord += 'y';
-                } else {
-                    return false;
-                }
-                
-            } else if (babbling.charAt(i) == 'w') {
-                
-                if (curWord.isEmpty()) {
-                    curWord += 'w';
-                } else {
-                    return false;
-                }
-                
-            } else if (babbling.charAt(i) == 'm') {
-                if (curWord.isEmpty()) {
-                    curWord += 'm';
-                } else {
-                    return false;
-                }
-            } else if (babbling.charAt(i) == 'e') {
-                if (curWord.equals("y")) {
-                    curWord += 'e';
-                    if (lastWord.equals(curWord))
-                        return false;
-                    else {
-                        lastWord = curWord;
-                        curWord = "";
-                    }
-                } else {
-                    return false;
-                }
-            } else if (babbling.charAt(i) == 'o') {
-                if (curWord.equals("w")) {
-                    curWord += 'o';
-                } else if (curWord.equals("wo")) {
-                    curWord += 'o';
-                    if (lastWord.equals(curWord)) {
-                        return false;
-                    } else {
-                        lastWord = curWord;
-                        curWord = "";
-                    }
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        
-        if (curWord.isEmpty())
-            return true;
-        else
-            return false;
     }
 }
