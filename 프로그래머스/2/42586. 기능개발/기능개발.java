f2321da63df15f3538e2418919e1a0d575db650a
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-02-19 10:50:41 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,43 +1,27 @@
 import java.util.*;
 
-class Data {
-    int progress;
-    int speed;
-    
-    Data (int progress, int speed) {
-        this.progress = progress;
-        this.speed = speed;
-    }
-}
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        Stack<Data> stack = new Stack<>();
-        for (int i = progresses.length - 1; i >= 0; i--)
-            stack.push(new Data(progresses[i], speeds[i]));
-        
         ArrayList<Integer> arrayList = new ArrayList<>();
-        while (!stack.isEmpty()) {
-            Data curData = stack.pop();
-            
-            int q = (100 - curData.progress) / curData.speed;
-            int r = (100 - curData.progress) % curData.speed;
+        
+        for (int i = 0; i < progresses.length; i++) {
+            int q = (100 - progresses[i]) / speeds[i];
+            int r = (100 - progresses[i]) % speeds[i];
             if (r != 0)
                 q++;
             
             int count = 1;
-            while (!stack.isEmpty()) {
-                Data nextData = stack.peek();
-                int q2 = (100 - nextData.progress) / nextData.speed;
-                int r2 = (100 - nextData.progress) % nextData.speed;
+            for(i++; i < progresses.length; i++) {
+                int q2 = (100 - progresses[i]) / speeds[i];
+                int r2 = (100 - progresses[i]) % speeds[i];
                 if (r2 != 0)
                     q2++;
                 
                 if (q >= q2) {
-                    stack.pop();
                     count++;
                 } else {
+                    i--;
                     break;
                 }
             }
