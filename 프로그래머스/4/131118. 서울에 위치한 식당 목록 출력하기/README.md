7ed70fed1d5a83ec191ed6030ef5e8c2d91bbb86
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-02-21 11:49:14 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 서울에 위치한 식당 목록 출력하기 - 131118 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 08일 20:55:00
+2026년 02월 21일 11:49:07
 
 ### 문제 설명
 
--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,27 +1,26 @@
-# 서울에 위치한 식당들
-# 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회
+-- 코드를 입력하세요
 
-SELECT
-    I.REST_ID AS REST_ID,
-    I.REST_NAME AS REST_NAME,
-    I.FOOD_TYPE AS FOOD_TYPE,
-    I.FAVORITES AS FAVORITES,
-    I.ADDRESS AS ADDRESS,
-    ROUND(R.AVG_REVIEW_SCORE,2) AS SCORE
-FROM
-    REST_INFO AS I
-INNER JOIN
+-- 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수
+SELECT 
+    C.REST_ID
+  , C.REST_NAME
+  , C.FOOD_TYPE
+  , C.FAVORITES
+  , C.ADDRESS
+  , B.REVIEW_SCORE2
+  FROM 
+    REST_INFO C
+INNER JOIN 
     (
     SELECT
-        REST_ID,
-        AVG(REVIEW_SCORE) AS AVG_REVIEW_SCORE
+        A.REST_ID
+      , ROUND(AVG(A.REVIEW_SCORE), 2) AS REVIEW_SCORE2
     FROM
-        REST_REVIEW
+        REST_REVIEW A
     GROUP BY
-        REST_ID
-    ) AS R ON I.REST_ID = R.REST_ID
+        A.REST_ID    
+    ) B ON C.REST_ID = B.REST_ID
 WHERE
-    I.ADDRESS LIKE '서울%'
+    SUBSTR(C.ADDRESS, 1, 2) = '서울'
 ORDER BY
-    R.AVG_REVIEW_SCORE DESC,
-    FAVORITES DESC
+    B.REVIEW_SCORE2 DESC, C.FAVORITES DESC;
