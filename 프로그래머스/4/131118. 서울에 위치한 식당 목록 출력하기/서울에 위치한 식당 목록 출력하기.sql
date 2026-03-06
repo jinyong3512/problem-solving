9d251c8f77e842bb870d39816e01329305237e69
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-03-06 23:46:13 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,27 +1,28 @@
-# 서울에 위치한 식당들
-# 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회
-
-SELECT
-    I.REST_ID AS REST_ID,
-    I.REST_NAME AS REST_NAME,
-    I.FOOD_TYPE AS FOOD_TYPE,
-    I.FAVORITES AS FAVORITES,
-    I.ADDRESS AS ADDRESS,
-    ROUND(R.AVG_REVIEW_SCORE,2) AS SCORE
-FROM
-    REST_INFO AS I
-INNER JOIN
-    (
+SELECT -- 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수
+    A.REST_ID,
+    A.REST_NAME,
+    A.FOOD_TYPE,
+    A.FAVORITES,
+    A.ADDRESS,
+    B.REVIEW_SCORE2 AS SCORE
+FROM 
+    REST_INFO A
+INNER JOIN (
+-- LEFT OUTER JOIN (
     SELECT
         REST_ID,
-        AVG(REVIEW_SCORE) AS AVG_REVIEW_SCORE
+        ROUND(AVG(REVIEW_SCORE), 2) AS REVIEW_SCORE2
     FROM
-        REST_REVIEW
+        REST_REVIEW 
     GROUP BY
-        REST_ID
-    ) AS R ON I.REST_ID = R.REST_ID
-WHERE
-    I.ADDRESS LIKE '서울%'
-ORDER BY
-    R.AVG_REVIEW_SCORE DESC,
-    FAVORITES DESC
+        REST_ID 
+    ) B
+    ON A.REST_ID = B.REST_ID
+WHERE -- 서울에 위치한 식당들
+    SUBSTR(A.ADDRESS, 1, 2) = '서울'
+ORDER BY -- 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순
+    SCORE DESC, FAVORITES DESC;
+    
+-- 1. REST_REVIEW를 GROUP BY 했을때는 REST_ID가 NULL인 애들도 있음
+-- 2. 근데 어차피 LEFT OUTER JOIN하면서 사라짐.
+-- 3. INNER JOIN 하면 문제 틀리는 이유는 REST_INFO에는 있는 REST_ID가 REST_REVIEW에는 없을수도 있음
