fcc6b084bbe7921acdeb3f192f722e619b80cfcd
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-03-22 01:09:58 +0900

    [level 3] Title: 등굣길, Time: 0.61 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,32 +1,47 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         
-        int[][] dp = new int[n+1][m+1];
+        boolean[][] waters = new boolean[n][m];
+        for (int i = 0; i < puddles.length; i++) {
+            waters[puddles[i][1] - 1][puddles[i][0] - 1] = true;
+        }
         
+        int[][] dp = new int[n][m];
         
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ;j++){
-                
-                boolean can = true;
-                for(int k = 0 ; k < puddles.length ; k++){
-                    if(puddles[k][0] == j && puddles[k][1] == i)
-                        can = false;
-                }
-                
-                if(!can)
-                    continue;
-                
-                if(i==1 && j==1)
-                    dp[1][1] =1;
-                else if (i == 1){
-                    dp[i][j] = dp[i][j-1];
-                }else if (j == 1){
-                    dp[i][j] = dp[i-1][j];
-                }else
-                    dp[i][j] = (dp[i-1][j]+dp[i][j-1])%1000000007;
+        dp[0][0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            if (waters[i][0]) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
             }
         }
         
-        return dp[n][m];
+        for (int j = 1; j < dp[0].length; j++) {
+            if (waters[0][j]) {
+                dp[0][j] = 0;
+            } else {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (waters[i][j]) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+                }
+            }
+        }
+        
+        // for (int i = 0; i < dp.length; i++) {
+        //     for (int j = 0; j < dp[i].length; j++) {
+        //         System.out.printf("%d ", dp[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+        
+        return dp[n - 1][m - 1];
     }
 }
