3379bb108598e3bec2c2152c10cc4995dc996a28
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-04-06 15:59:43 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,10 @@
--- 동물의 생물 종, 이름, 성별 및 중성화 여부
-SELECT ANIMAL_TYPE
-     , CASE 
-            WHEN NAME IS NULL THEN 'No name'
-            ELSE NAME
-       END
-     , SEX_UPON_INTAKE
-  FROM ANIMAL_INS
-ORDER BY ANIMAL_ID  
+SELECT
+    animal_type,
+    CASE 
+        WHEN name IS NULL THEN 'No name'
+        ELSE name
+    END,
+    sex_upon_intake
+FROM ANIMAL_INS
+ORDER BY
+    animal_id ASC
