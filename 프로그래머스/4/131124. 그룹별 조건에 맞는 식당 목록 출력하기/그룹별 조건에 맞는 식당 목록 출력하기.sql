1b94a6daa51472ea5b7c6d4ba3dedb1e9c450ec3
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-04-10 21:18:46 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,20 +1,39 @@
-SELECT A.MEMBER_NAME
-     , B.REVIEW_TEXT
-     , TO_CHAR(B.REVIEW_DATE, 'YYYY-MM-DD')
-  FROM MEMBER_PROFILE A
-       RIGHT OUTER JOIN REST_REVIEW B
-       ON A.MEMBER_ID = B.MEMBER_ID  
- WHERE B.MEMBER_ID IN (
-    SELECT MEMBER_ID
-      FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    HAVING COUNT(*) = (
-        SELECT MAX(CNT)
-          FROM (
-                SELECT COUNT(*) AS CNT
-                  FROM REST_REVIEW
-                GROUP BY MEMBER_ID
-               )
-    )
-)
-ORDER BY REVIEW_DATE, REVIEW_TEXT
+-- 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회
+
+
+SELECT -- 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력
+    MP.member_name,
+    RR4.review_text,
+    TO_CHAR(RR4.review_date, 'YYYY-MM-DD') AS REVIEW_DATE
+FROM MEMBER_PROFILE MP
+INNER JOIN (
+    
+    SELECT
+        *
+    FROM REST_REVIEW RR3
+    WHERE
+        RR3.member_id IN (
+        SELECT member_id
+        FROM (
+            SELECT
+                RR2.member_id,
+                COUNT(*) AS count
+            FROM REST_REVIEW RR2
+            GROUP BY
+                RR2.member_id
+        )
+        WHERE   
+            count = (
+            SELECT         
+                MAX(COUNT(*))
+            FROM REST_REVIEW RR
+            GROUP BY
+                RR.member_id
+            )
+        )
+    
+) RR4
+    ON MP.member_id = RR4.member_id
+ORDER BY -- 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬
+    RR4.review_date ASC,
+    RR4.review_text ASC
