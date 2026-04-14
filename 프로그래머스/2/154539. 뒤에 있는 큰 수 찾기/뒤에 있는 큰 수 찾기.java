4574b75a0db7dd0004e3ed909e34cb21df1f5bd8
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-04-14 17:04:05 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 86.23 ms, Memory: 236 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
-class Data{
-    int number, index;
-    Data (int number, int index) {
-        this.number = number;
-        this.index = index;
-    }
-}
-
 class Solution {
     public int[] solution(int[] numbers) {
+        
+        Deque<Integer> deque = new ArrayDeque<>();        
         int[] answer = new int[numbers.length];
         
-        Stack<Data> stack = new Stack<>();
-        
         for (int i = 0; i < numbers.length; i++) {
-            while (!stack.isEmpty() && stack.peek().number < numbers[i]) {
-                Data curData = stack.pop();
-                answer[curData.index] = numbers[i];
+            while (!deque.isEmpty() && numbers[deque.peekLast()] < numbers[i]) {
+                answer[deque.removeLast()] = numbers[i];
             }
-            stack.push(new Data(numbers[i], i));
+            deque.addLast(i);
         }
         
-        while (!stack.isEmpty()) {
-                Data curData = stack.pop();
-                answer[curData.index] = -1;
-            }
+        // for (int i = 0; i < answer.length; i++) {
+        //     System.out.printf("%d ", answer[i]);
+        // }
+        // System.out.println();
+        
+        while (!deque.isEmpty()) {
+            answer[deque.removeLast()] = -1;
+        }
         
         return answer;
     }
