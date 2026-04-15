91642b9447af92db2993c04a57cd27ff237851fb
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-04-15 11:13:35 +0900

    [level 2] Title: 더 맵게, Time: 1725.93 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        
+                
         PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        
         for (int i = 0; i < scoville.length; i++) {
             pQ.add(scoville[i]);
         }
         
-        while (true) {
-            if (pQ.peek() >= K)
-                break;
-            
+        int answer = 0;
+        
+        while (pQ.size() > 1 && pQ.peek() < K) {
             answer++;
-            if (pQ.size() > 1) {
-                int num1 = pQ.remove();    
-                int num2 = pQ.remove();
-                pQ.add(num1 + num2 * 2);
-                
-            } else
-                return -1;
+            
+            int first = pQ.remove();
+            int second = pQ.remove();
+            
+            pQ.add(first + second * 2);
+        }
+        
+        if (pQ.size() == 1 && pQ.peek() < K) {
+            return -1;
         }
         
         return answer;
