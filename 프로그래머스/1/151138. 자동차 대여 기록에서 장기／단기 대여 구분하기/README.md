50e4158f87a66bb890959ae68ba68ac7c8b30889
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-04-27 16:13:34 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/README.md
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자동차 대여 기록에서 장기/단기 대여 구분하기 - 151138 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151138) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151138?language=oracle) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: undefined, 시간: 
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-Empty
+합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2024년 07월 07일 22:52:02
+2026년 04월 27일 16:13:10
 
 ### 문제 설명
 
--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,16 +1,14 @@
 SELECT
-    HISTORY_ID,
-    CAR_ID,
-    DATE_FORMAT(START_DATE,'%Y-%m-%d') AS START_DATE,
-    DATE_FORMAT(END_DATE,'%Y-%m-%d') AS END_DATE,
-    IF(
-        DATEDIFF(END_DATE,START_DATE) +2 >= 30,
-        '장기 대여',
-        '단기 대여'
-    ) AS RENT_TYPE
-FROM
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE
-    DATE_FORMAT(START_DATE,'%Y-%m') = '2022-09'
+    history_id,
+    car_id,
+    TO_CHAR(start_date, 'YYYY-MM-DD'),
+    TO_CHAR(end_date, 'YYYY-MM-DD'),    
+    CASE 
+        WHEN end_date - start_date >= 29 THEN '장기 대여'
+        ELSE '단기 대여'
+    END AS RENT_TYPE    
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE 
+    TO_CHAR(start_date, 'YYYY-MM') = '2022-09'
 ORDER BY
-    HISTORY_ID DESC
+    history_id DESC
