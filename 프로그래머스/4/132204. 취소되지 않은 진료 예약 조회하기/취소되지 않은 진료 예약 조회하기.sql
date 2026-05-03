635e60b9fc91e3224a8ff58dfe267fd822654b64
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-05-03 12:03:07 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,24 +1,19 @@
-# 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시
-SELECT
-    A.APNT_NO,
-    P.PT_NAME,
-    P.PT_NO,
-    A.MCDP_CD,
-    D.DR_NAME,
-    A.APNT_YMD
-    
-FROM
-    PATIENT AS P
-INNER JOIN
-    (
-    SELECT
-        *
-    FROM
-        APPOINTMENT
-    WHERE
-        APNT_CNCL_YN = 'N' AND DATE_FORMAT(APNT_YMD,'%Y-%m-%d') = '2022-04-13' AND MCDP_CD = 'CS'
-    ) AS A ON P.PT_NO = A.PT_NO
-INNER JOIN
-    DOCTOR AS D ON A.MDDR_ID = D.DR_ID
-ORDER BY
-    A.APNT_YMD ASC
+-- 2022년 4월 13일 취소되지 않은 흉부외과(CS) 진료 예약 내역을 조회하는 SQL문을 작성
+SELECT -- 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시 항목이 출력
+    A.apnt_no,
+    P.pt_name,
+    P.pt_no,
+    D.mcdp_cd,
+    D.dr_name,
+    A.apnt_ymd
+FROM APPOINTMENT A
+INNER JOIN DOCTOR D
+    ON A.mddr_id = D.dr_id
+INNER JOIN PATIENT P
+    ON A.pt_no = P.pt_no    
+WHERE
+    TO_CHAR(A.apnt_ymd, 'YYYY-MM-DD') = '2022-04-13' AND
+    A.apnt_cncl_yn = 'N' AND
+    D.mcdp_cd = 'CS'
+ORDER BY -- 진료예약일시를 기준으로 오름차순
+    A.apnt_ymd ASC
