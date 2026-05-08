9c102b44b7594b5b911de48884889def14930edd
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-05-09 00:07:47 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.08 ms, Memory: 56.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        
+        // dp[0] = 0
+        // dp[1] = 1
+        // dp[2] = dp[1] + dp[0]
+                        
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp [i - 1]) % 1000000007;
+        }
+        
+        return dp[n];
+    }
+}
