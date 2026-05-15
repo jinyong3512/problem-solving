dcb6fecb3a96f6add3eb5c7bb3a1e5630209eadd
김진용 <88269663+jinyong3512@users.noreply.github.com>
2026-05-15 19:49:04 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/README.md
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 흉부외과 또는 일반외과 의사 목록 출력하기 - 132203 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132203?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132203?language=oracle) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: undefined, 시간: 
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-Empty
+합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2026년 03월 06일 22:19:39
+2026년 05월 15일 19:48:59
 
 ### 문제 설명
 
--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,12 +1,11 @@
-SELECT -- 의사의 이름, 의사ID, 진료과, 고용일자
-    DR_NAME,
-    DR_ID,
-    MCDP_CD,
-    DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD2
-FROM
-    DOCTOR
+SELECT
+    dr_name,
+    dr_id,
+    mcdp_cd,
+    TO_CHAR(hire_ymd, 'YYYY-MM-DD')
+FROM DOCTOR
 WHERE
-    MCDP_CD = 'CS'
-    OR MCDP_CD = 'GS'
-ORDER BY -- 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순
-    HIRE_YMD DESC, DR_NAME ASC;
+    mcdp_cd IN ('CS', 'GS')
+ORDER BY
+    hire_ymd DESC,
+    dr_name ASC
